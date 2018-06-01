87dd72a79bb0914f7458e8d96507103f0e0fa278
LiuLiang <LiuLiang@walkingtec.cn>
2018-06-02 00:04:20 +0800

    更新Doc项目

---
+++ WalkingTec.Mvvm.Doc/Models/StudentMajor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WalkingTec.Mvvm.Core;
+
+namespace WalkingTec.Mvvm.Doc.Models
+{
+    public class StudentMajor : BasePoco
+    {
+        public Major Major { get; set; }
+        public Student Student { get; set; }
+
+        public Guid MajorId { get; set; }
+
+        public Guid StudentId { get; set; }
+    }
+}
