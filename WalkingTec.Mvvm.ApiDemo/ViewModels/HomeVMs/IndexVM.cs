dcf74be572131b953940d56898a68a5cac460a40
LiuLiang <LiuLiang@walkingtec.cn>
2018-06-06 22:14:50 +0800

    加入了ApiDemo（未完成），修改左侧菜单显示图标的问题

---
+++ WalkingTec.Mvvm.ApiDemo/ViewModels/HomeVMs/IndexVM.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using WalkingTec.Mvvm.Core;
+
+namespace WalkingTec.Mvvm.ApiDemo.ViewModels.HomeVMs
+{
+    public class IndexVM : BaseVM
+    {
+        public List<TreeSelectListItem> Menu { get; set; }
+    }
+}
