2b08fc2c9979d3b12bbc85b55ba3dc16881a5dc4
LiuLiang <LiuLiang@walkingtec.cn>
2018-06-13 00:00:47 +0800

    修改默认项目模板左侧菜单没有过滤权限的问题

--- WalkingTec.Mvvm.Demo/ViewModels/HomeVMs/IndexVM.cs
+++ WalkingTec.Mvvm.Demo/ViewModels/HomeVMs/IndexVM.cs
@@ -1,10 +1,74 @@
 using System.Collections.Generic;
+using System.Linq;
 using WalkingTec.Mvvm.Core;
+using WalkingTec.Mvvm.Core.Extensions;
 
 namespace WalkingTec.Mvvm.Demo.ViewModels.HomeVMs
 {
     public class IndexVM : BaseVM
     {
-        public List<TreeSelectListItem> Menu { get; set; }
+        public List<FrameworkMenu> AllMenu { get; set; }
+
+        public List<TreeSelectListItem> Menu
+        {
+            get
+            {
+                return GetUserMenu();
+            }
+        }
+
+        public List<TreeSelectListItem> GetUserMenu()
+        {
+            if (ConfigInfo.IsQuickDebug == true)
+            {
+                return AllMenu.AsQueryable().GetTreeSelectListItems(null, null, x => x.PageName, null, x => x.IConId.ToString(), x => x.Url, SortByName: false); ;
+            }
+            else
+            {
+                var rv = AllMenu.Where(x =>x.ShowOnMenu == true).AsQueryable().GetTreeSelectListItems(null, null, x => x.PageName, null, x => x.IConId.ToString(), x => x.Url, SortByName: false);
+                RemoveUnAccessableMenu(rv, LoginUserInfo);
+                return rv;
+            }
+        }
+
+        /// <summary>
+        /// 移除没有权限访问的菜单
+        /// </summary>
+        /// <param name="menus">菜单列表</param>
+        /// <param name="info">用户信息</param>
+        private void RemoveUnAccessableMenu(List<TreeSelectListItem> menus, LoginUserInfo info)
+        {
+            if (menus == null)
+            {
+                return;
+            }
+
+            List<TreeSelectListItem> toRemove = new List<TreeSelectListItem>();
+            //如果没有指定用户信息，则用当前用户的登录信息
+            if (info == null)
+            {
+                info = LoginUserInfo;
+            }
+            //循环所有菜单项
+            foreach (var menu in menus)
+            {
+                //判断是否有权限，如果没有，则添加到需要移除的列表中
+                if (!string.IsNullOrEmpty(menu.Url) && info.IsAccessable(menu.Url) == false)
+                {
+                    toRemove.Add(menu);
+                }
+                //如果有权限，则递归调用本函数检查子菜单
+                else
+                {
+                    RemoveUnAccessableMenu(menu.Children, info);
+                }
+            }
+            //删除没有权限访问的菜单
+            foreach (var remove in toRemove)
+            {
+                menus.Remove(remove);
+            }
+        }
+
     }
 }
