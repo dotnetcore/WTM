5131434ffcebe95ae06f830700dcb4c097f4bba6
LiuLiang <LiuLiang@walkingtec.cn>
2018-07-11 00:09:12 +0800

    修改角色和用户组编码规则

--- WalkingTec.Mvvm.Core/Models/FrameworkGroup.cs
+++ WalkingTec.Mvvm.Core/Models/FrameworkGroup.cs
@@ -12,8 +12,8 @@ namespace WalkingTec.Mvvm.Core
     {
         [Display(Name = "用户组编码")]
         [Required(ErrorMessage = "{0}是必填项")]
-        [RegularExpression("^[0-9]{3,3}$", ErrorMessage = "{0}必须是3位数字")]
-        [StringLength(3)]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "{0}必须是数字")]
+        [StringLength(100, ErrorMessage = "{0}最大100位")]
         public string GroupCode { get; set; }
 
         [Display(Name = "用户组名称")]
