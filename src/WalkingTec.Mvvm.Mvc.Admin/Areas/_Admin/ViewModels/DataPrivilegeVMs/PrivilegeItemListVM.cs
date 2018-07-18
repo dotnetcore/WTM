55aa1e604a0379bfef382af8a10f3e9d5ee293d8
LiuLiang <LiuLiang@walkingtec.cn>
2018-07-19 01:07:48 +0800

    更新文档

---
+++ src/WalkingTec.Mvvm.Mvc.Admin/Areas/_Admin/ViewModels/DataPrivilegeVMs/PrivilegeItemListVM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WalkingTec.Mvvm.Core;
+
+namespace WalkingTec.Mvvm.Mvc.Admin.ViewModels.DataPrivilegeVMs
+{
+    public class PrivilegeItemListVM : BasePagedListVM<PrivilegeItemView, PrivilegeItemSearcher>
+    {
+
+        protected override IEnumerable<IGridColumn<PrivilegeItemView>> InitGridHeader()
+        {
+            return new List<GridColumn<PrivilegeItemView>>{
+                this.MakeGridHeader(x => x.Text, 200),
+            };
+        }
+
+        public override IOrderedQueryable<PrivilegeItemView> GetSearchQuery()
+        {
+            List<PrivilegeItemView> AllItems = new List<PrivilegeItemView>();
+            var dps = ConfigInfo.DataPrivilegeSettings.Where(x => x.ModelName == Searcher.TableName).SingleOrDefault();
+            if (dps != null)
+            {
+                AllItems = dps.GetItemList(DC, null).Select(x=> new PrivilegeItemView { Text = x.Text, Value = x.Value, ID = Guid.Parse(x.Value) }).ToList();
+            }
+
+            return AllItems.AsQueryable().OrderBy(x => x.Text);
+        }
+    }
+
+    public class PrivilegeItemView : TopBasePoco
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class PrivilegeItemSearcher : BaseSearcher
+    {
+        public string TableName { get; set; }
+    }
+}
