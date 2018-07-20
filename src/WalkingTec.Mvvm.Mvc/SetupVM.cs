0fd792f427b8cde37e899c2de0be65549ef5f8e3
Wenhao.Wu <wuwenhao0327@gmail.com>
2018-07-20 10:40:23 +0800

    release 发布v1.3.24  Changes to be committed: 	modified:   src/WalkingTec.Mvvm.Mvc/SetupVM.cs 	modified:   version.props

--- src/WalkingTec.Mvvm.Mvc/SetupVM.cs
+++ src/WalkingTec.Mvvm.Mvc/SetupVM.cs
@@ -14,7 +14,7 @@ namespace WalkingTec.Mvvm.Mvc
 
     public class SetupVM : BaseVM
     {
-        private string version = "1.3.23";
+        private string version = "1.3.24";
 
         public bool EnableLog { get; set; }
 
