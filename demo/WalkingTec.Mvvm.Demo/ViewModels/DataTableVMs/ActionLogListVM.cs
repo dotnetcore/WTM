47e4b24b4f06a8adc214fbe48185b06a34d4fd7d
LiuLiang <LiuLiang@walkingtec.cn>
2018-08-06 16:22:05 +0800

    更新Demo

---
+++ demo/WalkingTec.Mvvm.Demo/ViewModels/DataTableVMs/ActionLogListVM.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using WalkingTec.Mvvm.Core;
+
+namespace WalkingTec.Mvvm.Demo.ViewModels.DataTableVMs
+{
+    public class ActionLogListVM : BasePagedListVM<BasePoco,BaseSearcher>
+    {
+        public override DbCommand GetSearchCommand()
+        {
+            var cmd = ((DbContext)DC).Database.GetDbConnection().CreateCommand();
+            cmd.CommandText = "SP_ActionLogSearch";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            return cmd;
+        }
+    }
+}
