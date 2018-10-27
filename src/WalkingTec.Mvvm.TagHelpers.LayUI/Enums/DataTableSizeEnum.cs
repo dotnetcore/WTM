b9faf265285a49676d4a0adb5a389636e2335a3f
Wenhao.Wu <wuwenhao0327@gmail.com>
2018-10-27 22:23:11 +0800

    mod 修改DataTable组件，标准按钮可放在 ToolBar 上  Changes to be committed: 	modified:   src/WalkingTec.Mvvm.TagHelpers.LayUI/DataTableTagHelper.cs 	new file:   src/WalkingTec.Mvvm.TagHelpers.LayUI/Enums/DataTableSizeEnum.cs 	new file:   src/WalkingTec.Mvvm.TagHelpers.LayUI/Enums/DataTableSkinEnum.cs 	new file:   src/WalkingTec.Mvvm.TagHelpers.LayUI/Enums/HttpMethodEnum.cs 	new file:   src/WalkingTec.Mvvm.TagHelpers.LayUI/Enums/SortTypeEnum.cs

---
+++ src/WalkingTec.Mvvm.TagHelpers.LayUI/Enums/DataTableSizeEnum.cs
@@ -0,0 +1,17 @@
+﻿namespace WalkingTec.Mvvm.TagHelpers.LayUI
+{
+    /// <summary>
+    /// 表格尺寸枚举
+    /// </summary>
+    public enum DataTableSizeEnum
+    {
+        /// <summary>
+        /// 小尺寸
+        /// </summary>
+        SM = 0,
+        /// <summary>
+        /// 大尺寸
+        /// </summary>
+        LG
+    }
+}
---
+++ src/WalkingTec.Mvvm.TagHelpers.LayUI/Enums/DataTableSkinEnum.cs
@@ -0,0 +1,21 @@
+﻿namespace WalkingTec.Mvvm.TagHelpers.LayUI
+{
+    /// <summary>
+    /// 表格风格枚举
+    /// </summary>
+    public enum DataTableSkinEnum
+    {
+        /// <summary>
+        /// 行边框风格
+        /// </summary>
+        Line = 0,
+        /// <summary>
+        /// 列边框风格
+        /// </summary>
+        Row,
+        /// <summary>
+        /// 无边框风格
+        /// </summary>
+        Nob
+    }
+}
