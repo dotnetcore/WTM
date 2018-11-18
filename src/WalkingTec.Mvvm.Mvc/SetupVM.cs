a078dc1093b8a3dd493605fe71cc184f54d494d1
Wenhao.Wu <wuwenhao0327@gmail.com>
2018-11-19 03:00:57 +0800

    release 发布v2.1.4

--- src/WalkingTec.Mvvm.Mvc/SetupVM.cs
+++ src/WalkingTec.Mvvm.Mvc/SetupVM.cs
@@ -14,7 +14,7 @@ namespace WalkingTec.Mvvm.Mvc
 
     public class SetupVM : BaseVM
     {
-        private string version = "2.1.3";
+        private string version = "2.1.4";
 
         public bool EnableLog { get; set; }
 
