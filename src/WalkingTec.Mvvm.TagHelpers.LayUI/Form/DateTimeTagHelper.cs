40bf040bdca7c5c8a671cfe6125bd89276f6f215
Vito <wuwenhao0327@gmail.com>
2019-01-27 22:33:35 +0800

    mod 修改 DateTimeTagHelper  Changes to be committed: 	modified:   src/WalkingTec.Mvvm.Mvc/SetupVM.cs 	modified:   src/WalkingTec.Mvvm.TagHelpers.LayUI/Form/DateTimeTagHelper.cs 	modified:   version.props

--- src/WalkingTec.Mvvm.TagHelpers.LayUI/Form/DateTimeTagHelper.cs
+++ src/WalkingTec.Mvvm.TagHelpers.LayUI/Form/DateTimeTagHelper.cs
@@ -51,21 +51,25 @@ namespace WalkingTec.Mvvm.TagHelpers.LayUI
         /// <summary>
         /// Value
         /// </summary>
-        public DateTime? Value { get; set; }
+        public string Value { get; set; }
+
         /// <summary>
         /// 控件选择类型
         /// 默认值：date
         /// </summary>
         public DateTimeTypeEnum Type { get; set; }
+
         /// <summary>
         /// 开启时间范围选择，默认 分隔符为 ~
         /// 默认值：false
         /// </summary>
         public bool? Range { get; set; }
+
         /// <summary>
         /// 时间范围分隔符 默认 ~
         /// </summary>
         public string RangeSplit { get; set; }
+
         /// <summary>
         /// Max
         /// 1.  如果值为字符类型，则：年月日必须用 -（中划线）分割、时分秒必须用 :（半角冒号）号分割。这里并非遵循 format 设定的格式
@@ -73,6 +77,7 @@ namespace WalkingTec.Mvvm.TagHelpers.LayUI
         /// 3.  如果值为整数类型，且数字 ≥ 86400000，则数字代表时间戳，如：max: 4073558400000，即代表最大日期在：公元3000年1月1日
         /// </summary>
         public string Max { get; set; }
+
         /// <summary>
         /// Min
         /// 1.  如果值为字符类型，则：年月日必须用 -（中划线）分割、时分秒必须用 :（半角冒号）号分割。这里并非遵循 format 设定的格式
@@ -80,6 +85,7 @@ namespace WalkingTec.Mvvm.TagHelpers.LayUI
         /// 3.  如果值为整数类型，且数字 ≥ 86400000，则数字代表时间戳，如：max: 4073558400000，即代表最大日期在：公元3000年1月1日
         /// </summary>
         public string Min { get; set; }
+
         /// <summary>
         /// 自定义格式 默认值：yyyy-MM-dd
         /// yyyy    年份，至少四位数。如果不足四位，则前面补零
@@ -96,25 +102,30 @@ namespace WalkingTec.Mvvm.TagHelpers.LayUI
         /// s       秒数，允许一位数。
         /// </summary>
         public string Format { get; set; }
+
         /// <summary>
         /// 层叠顺序
         /// 类型：Number，默认值：66666666
         /// 一般用于解决与其它元素的互相被遮掩的问题。如果 position 参数设为 static 时，该参数无效。
         /// </summary>
         public int? ZIndex { get; set; }
+
         /// <summary>
         /// 是否显示底部栏
         /// </summary>
         public bool? ShowBottom { get; set; }
+
         /// <summary>
         /// 语言 默认CN
         /// </summary>
         public DateTimeLangEnum? Lang { get; set; }
+
         /// <summary>
         /// 是否显示公历节日
         /// 内置了一些我国通用的公历重要节日，通过设置 true 来开启。国际版不会显示。
         /// </summary>
         public bool? Calendar { get; set; }
+
         /// <summary>
         /// 标注重要日子
         /// 标注          格式                    说明
@@ -123,17 +134,20 @@ namespace WalkingTec.Mvvm.TagHelpers.LayUI
         /// 特定的日期   {'2017-8-21': '发布'}     -
         /// </summary>
         public Dictionary<string, string> Mark { get; set; }
+
         /// <summary>
         /// 控件初始打开的回调，控件在打开时触发。
         /// 回调返回2个参数：初始的日期时间对象、当前实例对象
         /// </summary>
         public string ReadyFunc { get; set; }
+
         /// <summary>
         /// 日期时间被切换后的回调
         /// 年月日时间被切换时都会触发。
         /// 回调返回4个参数，分别代表：生成的值、日期时间对象、结束的日期时间对象、当前实例对象
         /// </summary>
         public string ChangeFunc { get; set; }
+
         /// <summary>
         /// 控件选择完毕后的回调
         /// 点击日期、清空、现在、确定均会触发。
@@ -155,8 +169,15 @@ namespace WalkingTec.Mvvm.TagHelpers.LayUI
             output.TagMode = TagMode.StartTagOnly;
             output.Attributes.Add("type", "text");
             output.Attributes.Add("name", Field.Name);
-            Value = (Field.Model as DateTime?) ?? Value;
-            output.Attributes.Add("value", Value?.ToString(DateTimeFormatDic[Type]));
+            if (Field.ModelExplorer.ModelType == typeof(string))
+            {
+                Value = Field.Model?.ToString() ?? Value;
+            }
+            else
+            {
+                Value = (Field.Model as DateTime?)?.ToString(DateTimeFormatDic[Type]) ?? Value;
+            }
+            output.Attributes.Add("value", Value);
             output.Attributes.Add("class", "layui-input");
 
             if (Range.HasValue && Range.Value && string.IsNullOrEmpty(RangeSplit))
