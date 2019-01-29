faadca618c63b0595d1eb73cd339a55ea647b088
lengyxin <lengyingxin8966@gmail.com>
2019-01-29 23:59:36 +0800

    搜索优化

--- demo/WalkingTec.Mvvm.ApiDemo/ClientApp/src/components/dataView/header/search.tsx
+++ demo/WalkingTec.Mvvm.ApiDemo/ClientApp/src/components/dataView/header/search.tsx
@@ -1,77 +1,64 @@
 import { Button, Col, Divider, Form, Icon, Row } from 'antd';
 import { WrappedFormUtils } from 'antd/lib/form/Form';
-import { DesForm, DesError } from 'components/decorators';
+import { DesError } from 'components/decorators';
+import GlobalConfig from 'global.config';
 import lodash from 'lodash';
 import { action, observable } from 'mobx';
-import GlobalConfig from 'global.config';
 import { observer } from 'mobx-react';
 import * as React from 'react';
 import Store from 'store/dataSource';
 import "./style.less";
-
-// 偏移 1维  columnCount 二维 FormItems.length
-const offsetArray = [
-    [0, 0, 0, 0, 0],//0
-    [0, 0, 0, 0, 0],//1
-    [0, 0, 0, 0, 0],//2
-    [0, 0, 0, 0, 0],//3
-    [24, 18, 12, 6, 24],//4
-]
-const colLayout = {
-    xl: 6,
-    lg: 8,
-    md: 12
-}
-export interface IAppProps {
+interface IAppProps {
     /** 状态 */
     Store: Store;
-    /** 搜索表单 items */
-    FormItems: (props: WrappedFormUtils) => React.ReactNodeArray;
-    /** 搜索参数替换 */
-    onValueMap?: (vaule: Object) => Object;
-    /** 列 个数 默认 4 个 */
+    /** 表单 */
+    form: WrappedFormUtils;
+    /** 列数 */
     columnCount?: number;
+    /** 提交方法 */
+    onSubmit?: (event?) => void;
+    /** 重置方法 */
+    onReset?: (event?) => void;
     [key: string]: any;
 }
-
-/**
- * 装饰器
- * @param params 
- */
-export function DecoratorsSearch(params: IAppProps) {
-    return function (Component: any): any {
-        return class extends React.Component<any, any> {
-            render() {
-                return <DataViewSearch {...params} {...this.props}>
-                    <Component {...params} {...this.props} />
-                </DataViewSearch>
-            }
-        }
-    }
-}
-
 /**
  * 搜索标题组件 
  * 
  * 不要直接修改 wtm 组件 使用继承重写的方式修改
  */
 @DesError
-@DesForm
 @observer
 export class DataViewSearch extends React.Component<IAppProps, any> {
     Store: Store = this.props.Store;
     @observable toggle = false;
     columnCount = GlobalConfig.searchColumnCount || 3;
+    /**
+     * 提交表单
+     * @param e 
+     */
     onSubmit(e) {
+        if (this.props.onSubmit) {
+            return this.props.onSubmit(e)
+        }
         e.preventDefault();
+        e.stopPropagation();
         this.props.form.validateFields((err, values) => {
+            console.log("搜索参数", values);
             if (!err) {
-                values = this.props.onValueMap && this.props.onValueMap(values)
                 this.Store.onSearch(values)
             }
         });
     }
-    onReset() {
+    /**
+     * 重置表单
+     * @param e 
+     */
+    onReset(e) {
+        if (this.props.onReset) {
+            return this.props.onReset(e)
+        }
+        e.preventDefault();
+        e.stopPropagation();
         const { resetFields } = this.props.form;
         resetFields();
         this.props.form.validateFields((err, values) => {
@@ -87,15 +74,15 @@ export class DataViewSearch extends React.Component<IAppProps, any> {
     render() {
         let items = [],
             columnCount = this.props.columnCount || this.columnCount,
-            FormItems = this.props.FormItems(this.props.form),
-            toggleShow = FormItems.length >= columnCount,
+            childrens = React.Children.toArray(this.props.children),
+            toggleShow = childrens.length >= columnCount,
             colSpan = 24 / columnCount,//每列 值
             colSpanSearch = colSpan;
         // 展开收起
         if (this.toggle) {
-            items = FormItems;
+            items = childrens;
         } else {
-            items = [...FormItems].splice(0, columnCount - 1);
+            items = [...childrens].splice(0, columnCount - 1);
         }
         const itemslength = items.length
         // 列行数
@@ -107,9 +94,7 @@ export class DataViewSearch extends React.Component<IAppProps, any> {
         return (
             <Form className="data-view-search" onSubmit={this.onSubmit.bind(this)}>
                 <Row type="flex" >
-                    {items.map(x => {
-                        return <Col span={colSpan} key={x.key} >{x} </Col>
-                    })}
+                    {items.map(x => <Col key={x.key} span={colSpan}>{x}</Col>)}
                     <Col span={colSpanSearch} className="data-view-search-right" >
                         <Button icon="search" type="primary" htmlType="submit" loading={this.Store.pageState.loading}>搜索</Button>
                         <Divider type="vertical" />
