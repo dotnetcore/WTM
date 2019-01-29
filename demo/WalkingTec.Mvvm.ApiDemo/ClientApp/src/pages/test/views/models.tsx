2566bd48fae9e5db5544338eda5bfe7b53755462
lengyxin <lengyingxin8966@gmail.com>
2019-01-29 23:15:18 +0800

    扩展默认表单

--- demo/WalkingTec.Mvvm.ApiDemo/ClientApp/src/pages/test/views/models.tsx
+++ demo/WalkingTec.Mvvm.ApiDemo/ClientApp/src/pages/test/views/models.tsx
@@ -10,13 +10,13 @@ export default {
     /** Email */
     Email: <Input placeholder="请输入 Email" />,
     /** Name */
-    Name: <Input placeholder="请输入 Name"  />,
+    Name: <Input placeholder="请输入 Name" />,
     /** IsValid */
     IsValid: <Switch checkedChildren={<Icon type="check" />} unCheckedChildren={<Icon type="close" />} />,
     /** 照片 */
-    PhotoId: UploadImg,
+    PhotoId: <UploadImg />,
     /** 性别 */
-    Sex: <Select placeholder="性别"  showArrow allowClear>
+    Sex: <Select placeholder="性别" showArrow allowClear>
         <Select.Option value={0}>男</Select.Option>
         <Select.Option value={1}>女</Select.Option>
     </Select>
