68510bd163f5d23ac3c525df9b29adade846a92e
lengyxin <lengyingxin8966@gmail.com>
2019-01-29 23:59:36 +0800

    搜索优化

--- demo/WalkingTec.Mvvm.ApiDemo/ClientApp/src/pages/test/views/search.tsx
+++ demo/WalkingTec.Mvvm.ApiDemo/ClientApp/src/pages/test/views/search.tsx
@@ -1,64 +1,49 @@
 import { Form } from 'antd';
-import { DecoratorsSearch } from 'components/dataView/header/search';
-import { DesError } from 'components/decorators';
+import { DataViewSearch } from 'components/dataView/header/search';
+import { DesForm } from 'components/decorators';
 import GlobalConfig from 'global.config'; //全局配置
+import { observer } from 'mobx-react';
 import * as React from 'react';
 import Store from '../store';
 import Models from './models';
-const FormItem = Form.Item;
 const formItemLayout = {
     ...GlobalConfig.formItemLayout
 };
-/**
- * 搜索条件头
- */
-@DesError
-@DecoratorsSearch({
-    Store,
-    // columnCount:3,
-    onValueMap(values) {
-        console.log("搜索参数", values);
-        return values
-    },
-    FormItems: ({ getFieldDecorator }) => {
-        return [
-            <FormItem label="ITCode" key="ITCode" {...formItemLayout}>
+@DesForm
+@observer
+export default class extends React.Component<any, any> {
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return <DataViewSearch
+            // columnCount={4} 默认全局
+            Store={Store}
+            form={this.props.form}
+        >
+            <Form.Item label="ITCode" {...formItemLayout}>
                 {getFieldDecorator('ITCode', {
                     initialValue: Store.searchParams['ITCode'],
                 })(Models.ITCode)}
-            </FormItem>
-            ,
-            <FormItem label="Name" key="Name" {...formItemLayout}>
+            </Form.Item>
+            <Form.Item label="Name"  {...formItemLayout}>
                 {getFieldDecorator('Name', {
                     initialValue: Store.searchParams['Name'],
                 })(Models.Name)}
-            </FormItem>
-            ,
-            <FormItem label="性别" key="Sex" {...formItemLayout}>
+            </Form.Item>
+            <Form.Item label="性别" {...formItemLayout}>
                 {getFieldDecorator('Sex', {
                     initialValue: Store.searchParams['Sex'],
                 })(Models.Sex)}
-            </FormItem>
-            ,
-            <FormItem label="test2" key="test2" {...formItemLayout}>
+            </Form.Item>
+            <Form.Item label="test2"  {...formItemLayout}>
                 {getFieldDecorator('test2', {
                     initialValue: Store.searchParams['test2'],
                 })(Models.Name)}
-            </FormItem>
-            ,
-            <FormItem label="test3" key="test3" {...formItemLayout}>
+            </Form.Item>
+            <Form.Item label="test3" {...formItemLayout}>
                 {getFieldDecorator('test3', {
                     initialValue: Store.searchParams['test2'],
                 })(Models.Name)}
-            </FormItem>
-        ]
-    }
-})
-export default class extends React.Component<any, any> {
-    shouldComponentUpdate() {
-        return false
-    }
-    render() {
-        return null
+            </Form.Item>
+        </DataViewSearch>
     }
 }
