c3aff0b59ca0a1cbd9d40e344248091c6185e937
LengYXin <lengyingxin8966@gmail.com>
2019-02-20 00:40:50 +0800

    更改 item 禁用 模式

--- demo/WalkingTec.Mvvm.ApiDemo/ClientApp/src/components/dataView/other/formItem/index.tsx
+++ demo/WalkingTec.Mvvm.ApiDemo/ClientApp/src/components/dataView/other/formItem/index.tsx
@@ -20,8 +20,10 @@ interface IFormItemProps {
             formItem: React.ReactNode
         };
     };
-    /** 禁用 显示 span */
+    /** 禁用 控件输入更改 */
     disabled?: boolean;
+    /** 禁用 组件 显示 span */
+    display?: boolean;
     /** 获取默认值 */
     defaultValue?: boolean;
     /** 详情数据 和 defaultValue 共同使用 */
@@ -37,7 +39,7 @@ interface IFormItemProps {
 @observer
 export class FormItem extends React.Component<IFormItemProps, any> {
     render() {
-        const { form = {}, fieId, models, defaultValue, decoratorOptions, formItemProps, details, disabled, render } = this.props;
+        const { form = {}, fieId, models, defaultValue, decoratorOptions, formItemProps, details, disabled, display, render } = this.props;
         const { getFieldDecorator }: WrappedFormUtils = form;
         // 获取模型 item
         const model = lodash.get(models, fieId) || { rules: [], label: `未获取到模型(${fieId})`, formItem: <Input placeholder={`未获取到模型(${fieId})`} /> };
@@ -45,17 +47,18 @@ export class FormItem extends React.Component<IFormItemProps, any> {
             rules: model.rules,
             ...decoratorOptions
         };
-        // 获取默认值
-        if (defaultValue && typeof defaultValue === "boolean" || disabled && typeof disabled === "boolean") {
+        // 获取默认值 默认值，禁用，显示 span 
+        if (typeof defaultValue === "boolean" || typeof disabled === "boolean" || typeof display === "boolean") {
             options.initialValue = lodash.get(details, fieId);
         }
         let renderItem = null;
         // 重写渲染
-        if (render && typeof render === "function") {
+        if (typeof render === "function") {
             renderItem = render(options.initialValue, fieId);
         } else {
             // 禁用显示 span
-            if (disabled && typeof disabled === "boolean") {
+            if (typeof display === "boolean") {
+                // 数据 是 obj 类型转换 为 json 字符串，防止 react 报错
                 if (typeof options.initialValue === "object") {
                     renderItem = <span>{JSON.stringify(options.initialValue)}</span>
                 } else {
@@ -63,6 +66,10 @@ export class FormItem extends React.Component<IFormItemProps, any> {
                 }
             } else {
                 renderItem = getFieldDecorator && getFieldDecorator(this.props.fieId, options)(model.formItem);
+                // 禁用 输入控件
+                if (typeof disabled === "boolean") {
+                    renderItem = React.cloneElement(renderItem, { disabled: true })
+                }
             }
         }
         return <Form.Item label={model.label} {...formItemLayout}  {...formItemProps}>
