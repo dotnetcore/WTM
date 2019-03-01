45e2adcc506dd3fc4d9ac2a6f70eb9660839a37b
LiuLiang <LiuLiang@walkingtec.cn>
2019-03-01 16:39:46 +0800

    更新代码生成

---
+++ demo/WalkingTec.Mvvm.ApiDemo/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WalkingTec.Mvvm.Core;
+
+namespace WalkingTec.Mvvm.ApiDemo
+{
+    public class TestModel:BasePoco
+    {
+        public decimal Teset { get; set; }
+    }
+}
