88274bdc56256292320801930e1ca384768f778b
LiuLiang <LiuLiang@walkingtec.cn>
2019-03-07 14:00:56 +0800

    更新安装代码

--- src/WalkingTec.Mvvm.Mvc/Helper/FrameworkServiceExtension.cs
+++ src/WalkingTec.Mvvm.Mvc/Helper/FrameworkServiceExtension.cs
@@ -385,10 +385,10 @@ namespace WalkingTec.Mvvm.Mvc
                     }
                 }
             }
-            models.Add(typeof(FrameworkUserBase));
-            models.Add(typeof(FrameworkGroup));
-            models.Add(typeof(FrameworkRole));
-            models.Add(typeof(ActionLog));
+            //models.Add(typeof(FrameworkUserBase));
+            //models.Add(typeof(FrameworkGroup));
+            //models.Add(typeof(FrameworkRole));
+            //models.Add(typeof(ActionLog));
             return models;
         }
 
