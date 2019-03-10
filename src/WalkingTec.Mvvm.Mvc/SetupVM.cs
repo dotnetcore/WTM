2c49825598680ee84df776a38a97264b2fd3e81c
Vito <wuwenhao0327@gmail.com>
2019-03-11 00:15:38 +0800

    release 发布v2.2.18

--- src/WalkingTec.Mvvm.Mvc/SetupVM.cs
+++ src/WalkingTec.Mvvm.Mvc/SetupVM.cs
@@ -15,7 +15,7 @@ namespace WalkingTec.Mvvm.Mvc
 
     public class SetupVM : BaseVM
     {
-        private string version = "2.2.18-pre1";
+        private string version = "2.2.18";
 
         public bool EnableLog { get; set; }
 
