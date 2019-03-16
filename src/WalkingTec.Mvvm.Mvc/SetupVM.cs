d1913e6934cd5a9c6cbb6a2051ff5f47a9f85de1
Vito <wuwenhao0327@gmail.com>
2019-03-16 22:54:08 +0800

    mod 修改发布版本

--- src/WalkingTec.Mvvm.Mvc/SetupVM.cs
+++ src/WalkingTec.Mvvm.Mvc/SetupVM.cs
@@ -15,7 +15,7 @@ namespace WalkingTec.Mvvm.Mvc
 
     public class SetupVM : BaseVM
     {
-        private string version = "2.2.22-pre2";
+        private string version = "2.2.22-pre5";
 
         public bool EnableLog { get; set; }
 
