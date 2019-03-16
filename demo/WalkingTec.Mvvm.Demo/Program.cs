05cff70605944d3f69c189d5b8ac3351e162155d
Vito <wuwenhao0327@gmail.com>
2019-03-16 22:26:59 +0800

    add 添加端口监听

--- demo/WalkingTec.Mvvm.Demo/Program.cs
+++ demo/WalkingTec.Mvvm.Demo/Program.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+using System.IO;
+
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Filters;
-using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
 using WalkingTec.Mvvm.Core;
 using WalkingTec.Mvvm.Demo.Models;
 using WalkingTec.Mvvm.Mvc;
@@ -17,31 +20,48 @@ namespace WalkingTec.Mvvm.Demo
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args);
 
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureServices((hostingCtx, x) =>
-                {
-                    var pris = new List<IDataPrivilege>
-                    {
-                        new DataPrivilegeInfo<School>("学校", y => y.SchoolName),
-                        new DataPrivilegeInfo<Major>("专业", y => y.MajorName)
-                    };
-                    x.AddFrameworkService(dataPrivilegeSettings: pris,webHostBuilderContext:hostingCtx);
-                    x.AddLayui();
-                })
-                .Configure(x =>
-                {
-                    x.UseFrameworkService(routes =>
-                    {
-                        routes.MapRoute(
-                            name: "areaRoute",
-                            template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-                        routes.MapRoute(
-                            name: "default",
-                            template: "{controller=Home}/{action=Index}/{id?}");
-                    });
-                });
+            if (args != null) configurationBuilder.AddCommandLine(args);
 
+            var hostingConfig = configurationBuilder.Build();
+            var globalConfig = hostingConfig.Get<GlobalConfig>();
+
+            var ASPNETCORE_URLS = hostingConfig.GetValue<string>("ASPNETCORE_URLS");
+            if (!string.IsNullOrEmpty(ASPNETCORE_URLS))
+                globalConfig.ApplicationUrl = ASPNETCORE_URLS;
+
+            return
+                WebHost.CreateDefaultBuilder(args)
+                    .ConfigureServices((hostingCtx, x) =>
+                    {
+                        var pris = new List<IDataPrivilege>
+                        {
+                            new DataPrivilegeInfo<School>("学校", y => y.SchoolName),
+                            new DataPrivilegeInfo<Major>("专业", y => y.MajorName)
+                        };
+                        x.AddFrameworkService(dataPrivilegeSettings: pris, webHostBuilderContext: hostingCtx);
+                        x.AddLayui();
+                    })
+                    .Configure(x =>
+                    {
+                        x.UseFrameworkService(routes =>
+                        {
+                            routes.MapRoute(
+                                name: "areaRoute",
+                                template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                            routes.MapRoute(
+                                name: "default",
+                                template: "{controller=Home}/{action=Index}/{id?}");
+                        });
+                    })
+                    .UseUrls(globalConfig.ApplicationUrl);
+        }
     }
 }
