515c918bace6ff6ee2d4d8354a4a7af8bd2327e2
LengYXin <lengyingxin8966@gmail.com>
2019-04-03 17:48:55 +0800

    地址 url 更改

--- demo/WalkingTec.Mvvm.ApiDemo/ClientApp/src/pages/frameworkmenu/views/forms.tsx
+++ demo/WalkingTec.Mvvm.ApiDemo/ClientApp/src/pages/frameworkmenu/views/forms.tsx
@@ -21,12 +21,12 @@ export class InsertForm extends React.Component<any, any> {
             ...this.props,
             models: this.models,
         }
-        const IsInside = lodash.get<string>(this.props.form.getFieldsValue(), "Entity.IsInside", 'true');
-       return <InfoShellLayout>
-            <FormItem {...props} fieId="Entity.IsInside" layout="row" value={true} />
-            <FormItem {...props} fieId="Entity.Url" layout="row" hidden={IsInside=='true'} />
-            <FormItem {...props} fieId="SelectedModule" hidden={IsInside=='false'} />
-            <FormItem {...props} fieId="SelectedActionIDs" hidden={IsInside=='false'} />
+        const IsInside = lodash.eq(this.props.form.getFieldValue("Entity.IsInside") || 'true', 'true')
+        return <InfoShellLayout>
+            <FormItem {...props} fieId="Entity.IsInside" layout="row" value='true' />
+            <FormItem {...props} fieId="Entity.Url" layout="row" disabled={IsInside} />
+            <FormItem {...props} fieId="SelectedModule" hidden={!IsInside} />
+            <FormItem {...props} fieId="SelectedActionIDs" hidden={!IsInside} />
             <FormItem {...props} fieId="Entity.PageName" />
             <FormItem {...props} fieId="Entity.ParentId" />
             <FormItem {...props} fieId="Entity.FolderOnly" />
@@ -61,12 +61,12 @@ export class UpdateForm extends React.Component<WTM.FormProps, any> {
             models: this.models,
         }
         getFieldDecorator('Entity.ID', { initialValue: lodash.get(this.props.defaultValues, 'Entity.ID') })
-        const IsInside = lodash.get<string>(this.props.form.getFieldsValue(), "Entity.IsInside", 'true');
+        const IsInside = lodash.eq(this.props.form.getFieldValue("Entity.IsInside") || 'true', 'true')
         return <InfoShellLayout>
-            <FormItem {...props} fieId="Entity.IsInside" layout="row" value={true} />
-            <FormItem {...props} fieId="Entity.Url" layout="row" hidden={IsInside == 'true'} />
-            <FormItem {...props} fieId="SelectedModule" hidden={IsInside == 'false'} />
-            <FormItem {...props} fieId="SelectedActionIDs" hidden={IsInside == 'false'} />
+            <FormItem {...props} fieId="Entity.IsInside" layout="row" value='true' />
+            <FormItem {...props} fieId="Entity.Url" layout="row" disabled={IsInside} />
+            <FormItem {...props} fieId="SelectedModule" hidden={!IsInside} />
+            <FormItem {...props} fieId="SelectedActionIDs" hidden={!IsInside} />
             <FormItem {...props} fieId="Entity.PageName" />
             <FormItem {...props} fieId="Entity.ParentId" />
             <FormItem {...props} fieId="Entity.FolderOnly" />
