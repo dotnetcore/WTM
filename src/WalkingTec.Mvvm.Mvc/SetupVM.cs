f4a76ef662325e8c53684954daedeb67b1d17ab4
Vito <wuwenhao0327@gmail.com>
2019-04-09 13:59:21 +0800

    release 发布v2.2.28

--- src/WalkingTec.Mvvm.Mvc/SetupVM.cs
+++ src/WalkingTec.Mvvm.Mvc/SetupVM.cs
@@ -15,7 +15,7 @@ namespace WalkingTec.Mvvm.Mvc
 
     public class SetupVM : BaseVM
     {
-        private string version = "2.2.27";
+        private string version = "2.2.28";
 
         public bool EnableLog { get; set; }
 
