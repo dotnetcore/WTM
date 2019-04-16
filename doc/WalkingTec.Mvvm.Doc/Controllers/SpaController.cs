bca5f35ec72fa703a1259b975a396f38153d6514
Liuliang <LiuLiang79@live.com>
2019-04-16 13:20:51 +0800

    更新doc

---
+++ doc/WalkingTec.Mvvm.Doc/Controllers/SpaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WalkingTec.Mvvm.Core;
+using WalkingTec.Mvvm.Mvc;
+
+namespace WalkingTec.Mvvm.Doc.Controllers
+{
+    [Public]
+    [ActionDescription("前后端分离（React）")]
+    public class SpaController : BaseController
+    {
+        [ActionDescription("介绍")]
+        public IActionResult Intro()
+        {
+            return PartialView();
+        }
+
+        [ActionDescription("全局配置")]
+        public IActionResult Global()
+        {
+            return PartialView();
+        }
+
+        [ActionDescription("文件结构")]
+        public IActionResult Dir()
+        {
+            return PartialView();
+        }
+    }
+}
