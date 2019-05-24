ddaa3400ceb59ff9e5a48afd9d85d9ed5e157411
Liuliang <LiuLiang79@live.com>
2019-05-25 02:31:25 +0800

    update menuapi

---
+++ src/WalkingTec.Mvvm.Core/Support/EFLogger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalkingTec.Mvvm.Core
+{
+    public class EFLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName) => new EFLogger(categoryName);
+        public void Dispose() { }
+    }
+
+    public class EFLogger : ILogger
+    {
+        private readonly string categoryName;
+
+        public EFLogger(string categoryName) => this.categoryName = categoryName;
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            //ef core执行数据库查询时的categoryName为Microsoft.EntityFrameworkCore.Database.Command,日志级别为Information
+            if (categoryName == "Microsoft.EntityFrameworkCore.Database.Command"
+                    && logLevel == LogLevel.Information)
+            {
+                var logContent = formatter(state, exception);
+                //TODO: 拿到日志内容想怎么玩就怎么玩吧
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(logContent);
+                Console.ResetColor();
+            }
+        }
+
+        public IDisposable BeginScope<TState>(TState state) => null;
+    }
+}
