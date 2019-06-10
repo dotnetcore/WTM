44e985d7aff06b2815829a1886aab19cf55307e0
LengYXin <lengyingxin8966@gmail.com>
2019-06-10 13:56:13 +0800

    菜单优化。

---
+++ demo/WalkingTec.Mvvm.ReactDemo/ClientApp/src/app/pages/demo/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+export default class App extends React.Component<any, any> {
+  public render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
