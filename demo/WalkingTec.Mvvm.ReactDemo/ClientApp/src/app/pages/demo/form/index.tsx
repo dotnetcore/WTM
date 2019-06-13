2e5edf83a37f9dfdb0fb4a4494fb3b8af29b6935
LengYXin <lengyingxin8966@gmail.com>
2019-06-13 14:27:21 +0800

    时间区间

--- demo/WalkingTec.Mvvm.ReactDemo/ClientApp/src/app/pages/demo/form/index.tsx
+++ demo/WalkingTec.Mvvm.ReactDemo/ClientApp/src/app/pages/demo/form/index.tsx
@@ -1,10 +1,8 @@
-import * as React from 'react';
-import { Observable } from 'rxjs';
-
-import { DialogForm, DialogFormDes, DialogFormSubmit, FormItem, InfoShellLayout, DialogLoadData, } from 'components/dataView';
+import { FormItem, InfoShellLayout } from 'components/dataView';
 import { DesForm } from 'components/decorators';
-
-import { WtmEditor } from 'components/form';
+import { WtmEditor, WtmDatePicker, WtmRangePicker } from 'components/form';
+import * as React from 'react';
+import { Button, DatePicker } from 'antd';
 // @DialogFormDes({
 //     onFormSubmit(values) {
 //         return new Observable<boolean>(sub => {
@@ -15,11 +13,67 @@ import { WtmEditor } from 'components/form';
 // })
 @DesForm
 export default class App extends React.Component<any, any> {
+    models: WTM.FormItem = {
+        "WtmEditor": {
+            label: "富文本",
+            rules: [],
+            formItem: <WtmEditor />
+        },
+        "WtmDatePicker": {
+            label: "WTM日历",
+            rules: [],
+            formItem: <WtmDatePicker />
+        },
+        "WtmDatePicker2": {
+            label: "WTM日历时间",
+            rules: [],
+            formItem: <WtmDatePicker showTime />
+        },
+        "DatePicker": {
+            label: "Antd日历",//返回 Moment 对象
+            rules: [],
+            formItem: <DatePicker />
+        },
+        "DatePicker2": {
+            label: "Antd日历时间",//返回 Moment 对象
+            rules: [],
+            formItem: <DatePicker showTime />
+        },
+        "WtmRangePicker": {
+            label: "WTM区间",
+            rules: [],
+            formItem: <WtmRangePicker />
+        },
+        "WtmRangePicker2": {
+            label: "WTM区间 时间",
+            rules: [],
+            formItem: <WtmRangePicker showTime />
+        },
+    }
+    onSubmit() {
+        this.props.form.validateFields((err, values) => {
+            console.log("TCL: App -> onSubmit -> values", values)
+        });
+    }
     render() {
-        console.log(this)
+        const props = {
+            ...this.props,
+            models: this.models,
+        }
         return (
             <div>
-                <WtmEditor />
+                <div>
+                    <Button onClick={this.onSubmit.bind(this)}>打印数据（控制台）</Button>
+                </div>
+                <InfoShellLayout>
+                    <FormItem fieId="WtmDatePicker" {...props} value="2018-01-01"/>
+                    <FormItem fieId="WtmDatePicker2" {...props} />
+                    <FormItem fieId="DatePicker" {...props} />
+                    <FormItem fieId="DatePicker2" {...props} />
+                    <FormItem fieId="WtmRangePicker" {...props} />
+                    <FormItem fieId="WtmRangePicker2" {...props} />
+                    <FormItem fieId="WtmEditor" {...props} layout="row" />
+                </InfoShellLayout>
             </div>
         );
     }
