9adc7969c7c2ceba39e3139a0281737720cb7ce8
LengYXin <lengyingxin8966@gmail.com>
2019-06-13 14:27:21 +0800

    时间区间

--- demo/WalkingTec.Mvvm.ReactDemo/ClientApp/src/components/form/datePicker/index.tsx
+++ demo/WalkingTec.Mvvm.ReactDemo/ClientApp/src/components/form/datePicker/index.tsx
@@ -10,13 +10,14 @@ import { DatePickerProps } from 'antd/lib/date-picker/interface';
 import lodash from 'lodash';
 import moment from 'moment';
 import * as React from 'react';
+const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
 
 
-interface IAppProps extends DatePickerProps {
+interface IWtmDatePickerProps extends DatePickerProps {
   display?: boolean;
   [key: string]: any;
 }
-export class WtmDatePicker extends React.Component<IAppProps, any> {
+export class WtmDatePicker extends React.Component<IWtmDatePickerProps, any> {
   static wtmType = "DatePicker";
   onChange(data, dataString) {
     const onChange: any = this.props.onChange;
@@ -44,8 +45,45 @@ export class WtmDatePicker extends React.Component<IAppProps, any> {
         props.defaultValue = moment(props.value, props.format)
       }
     }
+    console.log("TCL: WtmRangePicker -> render -> props", this.props)
     delete props.value;
     return (<DatePicker  {...props} onChange={this.onChange.bind(this)} />);
+  }
+}
+
+interface IWtmRangePickerProps extends DatePickerProps {
+  display?: boolean;
+  [key: string]: any;
+}
+export class WtmRangePicker extends React.Component<IWtmRangePickerProps, any> {
+  static wtmType = "RangePicker";
+  onChange(data, dataString) {
+    const onChange: any = this.props.onChange;
+    onChange && onChange(dataString, data)
+  }
+  getFormat() {
+    const { showTime, format } = this.props;
+    if (format) {
+      return format
+    }
+    return showTime ? "YYYY-MM-DD HH:mm:ss" : "YYYY-MM-DD";
+  }
+  render() {
+    let props: any = {
+      format: this.getFormat(),
+      ...this.props
+    }
+    if (this.props.display) {
+      return <span>{moment(props.value, props.format).format(props.format)}</span>
+    }
+    if (props.value) {
+      // 转换 默认值
+      if (lodash.isString(props.value)) {
+        props.defaultValue = moment(props.value, props.format)
+      }
+    }
+    delete props.value;
+    return (<RangePicker  {...props} onChange={this.onChange.bind(this)} />);
   }
 }
 export default WtmDatePicker
