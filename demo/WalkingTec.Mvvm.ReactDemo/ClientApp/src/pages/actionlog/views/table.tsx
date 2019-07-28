770ed460b639c1173f7eafa5eb6e98b814b0c730
LengYXin <lengyingxin8966@gmail.com>
2019-07-28 13:54:24 +0800

    update 升级 table 模块 未 aggrid 组件

--- demo/WalkingTec.Mvvm.ReactDemo/ClientApp/src/pages/actionlog/views/table.tsx
+++ demo/WalkingTec.Mvvm.ReactDemo/ClientApp/src/pages/actionlog/views/table.tsx
@@ -1,103 +1,54 @@
-﻿import { columnsRender, columnsRenderImg, DataViewTable } from 'components/dataView';
-import { DesError } from 'components/decorators';
+﻿import { ColDef, ColGroupDef } from 'ag-grid-community';
+import { AgGrid } from 'components/dataView';
 import React from 'react';
 import Store from '../store';
 import Action from './action';
-/**
- * 列 信息配置
- * 完整参数列表 https://ant.design/components/table-cn/#components-table-demo-dynamic-settings
- * dataIndex:属性名称 区分大小写
- * title:表格显示的中文标题
- */
-const columns = [
-
+// 列配置
+const columnDefs: (ColDef | ColGroupDef)[] = [
     {
-        dataIndex: "LogType",
-        title: "类型",
-        render: columnsRender,
-        width:80
-    },
-   {
-        dataIndex: "ModuleName",
-        title: "模块",
-       render: columnsRender,
-       width: 120
-    },
-
-    {
-        dataIndex: "ActionName",
-        title: "动作",
-        render: columnsRender,
-        width: 120
-    },
-
-    {
-        dataIndex: "ITCode",
-        title: "ITCode",
-        render: columnsRender,
-        width: 120
-    },
-
-    {
-        dataIndex: "ActionUrl",
-        title: "Url",
-        render: columnsRender,
-        width: 200
-    },
-
-    {
-        dataIndex: "ActionTime",
-        title: "操作时间",
-        render: columnsRender,
-        width: 200
-    },
-
-    {
-        dataIndex: "Duration",
-        title: "时长",
-        render: columnsRender,
-        width: 100
-    },
-    
-
-    {
-        dataIndex: "IP",
-        title: "IP",
-        render: columnsRender,
-        width: 120
+        headerName: "类型", field: "LogType"
     },
     {
-        dataIndex: "Remark",
-        title: "备注",
-        render: columnsRender 
-    }
+        headerName: "模块", field: "ModuleName",
+    },
+    {
+        headerName: "动作", field: "ActionName",
+    },
+    {
+        headerName: "ITCode", field: "ITCode",
+    },
+    {
+        headerName: "Url", field: "ActionUrl",
+    },
+    {
+        headerName: "操作时间", field: "ActionTime",
+    },
+    {
+        headerName: "时长", field: "Duration",
+    },
+    {
+        headerName: "IP", field: "IP",
+    },
+    {
+        headerName: "备注", field: "Remark",
+    },
 ]
-
 /**
  * 表格
  */
-@DesError
 export default class extends React.Component<any, any> {
-    /**
-     * 操作动作
-     */
-    renderColumns() {
-        const tableColumns: any[] = [...columns];
-        // 根据需求 加入行动作
-        if (true) {
-            tableColumns.push(
-                {
-                    title: '动作',
-                    dataIndex: 'Action',
-                    fixed: 'right',//固定 列
-                    width: 150,
-                    render: (text, record) => <Action.rowAction data={record} />
-                }
-            )
-        }
-        return tableColumns
-    }
     render() {
-        return <DataViewTable Store={Store} columns={this.renderColumns()} />
+        return <AgGrid
+            // 页面状态 
+            Store={Store}
+            // 列配置
+            columnDefs={columnDefs}
+            // 行操作 
+            rowAction={Action.rowAction}
+            // 行操作 col props 同 columnDefs配置相同
+            // rowActionCol={{ headerName: "操作" }}
+            // frameworkComponents={{
+            // }}
+        />
     }
 }
