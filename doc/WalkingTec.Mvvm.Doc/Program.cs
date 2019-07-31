f2c4e32681a013e8d156a4542634f6e1a5c62aa4
Vito <wuwenhao0327@gmail.com>
2019-07-31 19:40:52 +0800

    feature(doc): remove the listening port  Changes to be committed: 	modified:   doc/WalkingTec.Mvvm.Doc/Program.cs

--- doc/WalkingTec.Mvvm.Doc/Program.cs
+++ doc/WalkingTec.Mvvm.Doc/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+
 using WalkingTec.Mvvm.Mvc;
 using WalkingTec.Mvvm.TagHelpers.LayUI;
 
@@ -30,7 +24,6 @@ namespace WalkingTec.Mvvm.Doc
                 {
                     x.UseFrameworkService();
                 })
-                .UseUrls("http://*:80")
                 .Build();
     }
 }
