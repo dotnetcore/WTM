4a9adf9dec5288cddaf70ac9495a9c0f97ea2f58
Liuliang <LiuLiang79@live.com>
2019-08-19 03:11:13 +0800

    feature: add tree doc

--- doc/WalkingTec.Mvvm.Doc/Controllers/UIController.cs
+++ doc/WalkingTec.Mvvm.Doc/Controllers/UIController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using WalkingTec.Mvvm.Core;
 using WalkingTec.Mvvm.Doc.FrameworkUserVms;
 using WalkingTec.Mvvm.Doc.Models;
+using WalkingTec.Mvvm.Doc.ViewModels.DepartmentVms;
 using WalkingTec.Mvvm.Doc.ViewModels.MajorVms;
 using WalkingTec.Mvvm.Doc.ViewModels.SchoolVms;
 using WalkingTec.Mvvm.Doc.ViewModels.StudentVms;
@@ -127,6 +128,13 @@ namespace WalkingTec.Mvvm.Doc.Controllers
         public IActionResult Slider()
         {
             var vm = CreateVM<StudentVm>();
+            return PartialView(vm);
+        }
+
+        [ActionDescription("Slider")]
+        public IActionResult Tree()
+        {
+            var vm = CreateVM<DepartmentVM>();
             return PartialView(vm);
         }
 
