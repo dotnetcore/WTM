88851b12d3777245c443def49c196d7359d07c2e
Liuliang <LiuLiang79@live.com>
2019-08-29 23:52:14 +0800

    fix: update doc and proj file

--- doc/WalkingTec.Mvvm.Doc/wwwroot/json/menu.js
+++ doc/WalkingTec.Mvvm.Doc/wwwroot/json/menu.js
@@ -5,7 +5,7 @@
                 {
                     "name": "QuickStart",
                     "title": "快速开始",
-                    "icon": "layui-icon layui-icon-home",
+                    "icon": "_wtmicon _wtmicon-dingwei",
                     "list": [
                         { "name": "/FirstProject", "title": "第一个项目", "jump": "/QuickStart/FirstProject" },
                         { "name": "/FirstModule", "title": "第一个模块", "jump": "/QuickStart/FirstModule" },
@@ -17,7 +17,7 @@
                 {
                     "name": "Global",
                     "title": "框架配置",
-                    "icon": "layui-icon layui-icon-home",
+                    "icon": "layui-icon layui-icon-template-1",
                     "list": [
                         { "name": "/Config", "title": "配置文件", "jump": "/Global/Config" },
                         { "name": "/Global", "title": "全局变量", "jump": "/Global/Global" },
@@ -40,7 +40,7 @@
                 {
                     "name": "VM",
                     "title": "视图模型层",
-                    "icon": "layui-icon layui-icon-home",
+                    "icon": "_wtmicon _wtmicon-zuzhiqunzu",
                     "list": [
                         { "name": "/VM", "title": "基础VM", "jump": "/VM/VM" },
                         { "name": "/CRUD", "title": "数据操作VM", "jump": "/VM/CRUD" },
@@ -58,7 +58,7 @@
                 {
                     "name": "Control",
                     "title": "控制器层",
-                    "icon": "layui-icon layui-icon-home",
+                    "icon": "layui-icon layui-icon-engine",
                     "list": [
                         { "name": "/Intro", "title": "介绍", "jump": "/Control/Intro" },
                         { "name": "/Att", "title": "属性", "jump": "/Control/Att" }
@@ -67,7 +67,7 @@
                 {
                     "name": "Data",
                     "title": "数据层",
-                    "icon": "layui-icon layui-icon-home",
+                    "icon": "_wtmicon _wtmicon-chucun",
                     "list": [
                         { "name": "/Intro", "title": "介绍", "jump": "/Data/Intro" },
                         { "name": "/Migration", "title": "迁移", "jump": "/Data/Migration" }
@@ -76,7 +76,7 @@
                 {
                     "name": "Spa",
                     "title": "前后端分离(React)",
-                    "icon": "layui-icon layui-icon-home",
+                    "icon": "layui-icon layui-icon-template",
                     "list": [
                         { "name": "/Intro", "title": "介绍", "jump": "/Spa/Intro" },
                         { "name": "/Global", "title": "配置", "jump": "/Spa/Global" },
@@ -86,7 +86,7 @@
                 {
                     "name": "UI",
                     "title": "页面层(LayUI)",
-                    "icon": "layui-icon layui-icon-home",
+                    "icon": "layui-icon layui-icon-layouts",
                     "list": [
                         { "name": "/Intro", "title": "介绍", "jump": "/UI/Intro" },
                         { "name": "/Layout", "title": "布局", "jump": "/UI/Layout" },
@@ -114,7 +114,7 @@
                 {
                     "name": "Admin",
                     "title": "框架管理模块",
-                    "icon": "layui-icon layui-icon-home",
+                    "icon": "_wtmicon _wtmicon-yingyongguanli",
                     "list": [
                         { "name": "Intro", "title": "介绍", "jump": "/Admin/Intro" },
                         { "name": "Log", "title": "日志管理", "jump": "/Admin/Log" },
