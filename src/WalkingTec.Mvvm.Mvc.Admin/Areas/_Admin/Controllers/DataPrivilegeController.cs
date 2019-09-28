7cb58fc0eb0fbe93880751e8cd95e70bc01b21c7
Liuliang <LiuLiang79@live.com>
2019-09-28 22:14:36 +0800

    fix: fix the menuicon bug and export file name error

--- src/WalkingTec.Mvvm.Mvc.Admin/Areas/_Admin/Controllers/DataPrivilegeController.cs
+++ src/WalkingTec.Mvvm.Mvc.Admin/Areas/_Admin/Controllers/DataPrivilegeController.cs
@@ -114,7 +114,7 @@ namespace WalkingTec.Mvvm.Mvc.Admin.Controllers
         {
             vm.SearcherMode = vm.Ids != null && vm.Ids.Count > 0 ? ListVMSearchModeEnum.CheckExport : ListVMSearchModeEnum.Export;
             var data = vm.GenerateExcel();
-            return File(data, "application/vnd.ms-excel", $"Export_ActionLog_{DateTime.Now.ToString("yyyy-MM-dd")}.xls");
+            return File(data, "application/vnd.ms-excel", $"Export_DataPrivilege_{DateTime.Now.ToString("yyyy-MM-dd")}.xls");
         }
     }
 }
