7afcec6881ad1df5ab14c2fd2dbf1aacabb3ab54
Liuliang <LiuLiang79@live.com>
2019-11-05 21:53:26 +0800

    fix: prepare for publish

--- src/WalkingTec.Mvvm.Core/Auth/CookieAuth/CookieOptions.cs
+++ src/WalkingTec.Mvvm.Core/Auth/CookieAuth/CookieOptions.cs
@@ -4,9 +4,9 @@ namespace WalkingTec.Mvvm.Core.Auth
 {
     public class CookieOptions
     {
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-        public int Expires { get; set; }
+        public string Issuer { get; set; } = "http://localhost";
+        public string Audience { get; set; } = "http://localhost";
+        public int Expires { get; set; } = 3600;
         public bool SlidingExpiration { get; set; } = true;
         public string LoginPath { get; set; } = CookieAuthenticationDefaults.LoginPath;
         public string LogoutPath { get; set; } = CookieAuthenticationDefaults.LogoutPath;
