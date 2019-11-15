175533f35ce2a25671cb55d825e76f5b32c26977
Liuliang <LiuLiang79@live.com>
2019-11-15 21:36:51 +0800

    fix: update mysql

--- src/WalkingTec.Mvvm.Mvc.Admin/ApiControllers/AccountController.cs
+++ src/WalkingTec.Mvvm.Mvc.Admin/ApiControllers/AccountController.cs
@@ -185,7 +185,7 @@ namespace WalkingTec.Mvvm.Admin.Api
                     Text = x.PageName,
                     Url = x.Url,
                     Icon = x.ICon
-                });
+                }).ToList();
                 ms.AddRange(folders);
                 foreach (var item in menus)
                 {
