0185086e8dea14965c6471326f9ac2bcc691003a
LengYXin <lengyingxin8966@gmail.com>
2019-12-10 11:24:33 +0800

    test:user login

--- demo/WalkingTec.Mvvm.Next/ClientApp/packages/public/src/entities/user/behavior.ts
+++ demo/WalkingTec.Mvvm.Next/ClientApp/packages/public/src/entities/user/behavior.ts
@@ -1,6 +1,7 @@
 import { action } from 'mobx';
 import { timer } from 'rxjs';
 import Entities from './entities';
+import { Request } from '../../utils/request';
 /**
  * 对象 动作 行为 
  * @export
@@ -8,18 +9,28 @@ import Entities from './entities';
  * @extends {Entities}
  */
 export default class EntitiesUserBehavior extends Entities {
+    Request = new Request();
     /**
      * 用户登录
-     * @param {*} UserName 用户名
-     * @param {*} Password 密码
+     * @param {*} userid 用户名
+     * @param {*} password 密码
      * @memberof EntitiesUserBehavior
      */
     @action
-    async  onLogin(UserName, Password) {
+    async  onLogin(userid, password) {
         this.Loading = true;
         // 模拟一个等待 1秒钟
-        const res = await timer(1000).toPromise();
-        this.onVerifyingLanding(UserName, Password);
+        // const res = await timer(1000).toPromise();
+        const res = await this.Request.ajax(
+            {
+                method: "post",
+                url: "/api/_login/Login",
+                body: { userid, password },
+                headers: { 'Content-Type': null }
+            }
+        ).toPromise();
+        console.log("TCL: EntitiesUserBehavior -> onLogin -> res", res)
+        this.onVerifyingLanding(userid, password);
     }
     /**
      * 验证登陆
--- demo/WalkingTec.Mvvm.Next/ClientApp/packages/public/src/entities/user/t.test.ts
+++ demo/WalkingTec.Mvvm.Next/ClientApp/packages/public/src/entities/user/t.test.ts
@@ -1,7 +1,8 @@
 import { EntitiesUserStore } from './index'
 jest.useFakeTimers()
-it('test', async () => {
+it('用户登录：', async () => {
     const Store = new EntitiesUserStore();
-    Store.onLogin('LENG', '000000');
+    Store.Request.target = 'http://localhost:5555';
+    await Store.onLogin('admin', '000000');
     console.log(`'状态：${Store.OnlineState} 年龄：${Store.Age} 姓名：${Store.Name}`)
 });
