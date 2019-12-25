7c26c7550dc01c41515ac316d7499da62432ead4
LengYXin <lengyingxin8966@gmail.com>
2019-12-25 21:13:15 +0800

    update:git

--- demo/WalkingTec.Mvvm.Next/ClientApp/packages/www-vue/src/views/Home.vue
+++ demo/WalkingTec.Mvvm.Next/ClientApp/packages/www-vue/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld,
+    HelloWorld
+  },
+  mounted() {
+    console.warn("TCL: HelloWorld -> mounted -> mounted");
   }
-}
+};
 </script>
