5c731984a6641e61352860b2aee61372ffb2481a
Liuliang <LiuLiang79@live.com>
2020-02-06 21:15:29 +0800

    fix: fix dataprivilege api

--- src/WalkingTec.Mvvm.Mvc.Admin/ApiControllers/DataPrivilegeController.cs
+++ src/WalkingTec.Mvvm.Mvc.Admin/ApiControllers/DataPrivilegeController.cs
@@ -92,16 +92,16 @@ namespace WalkingTec.Mvvm.Admin.Api
 
         [HttpPost("[action]")]
         [ActionDescription("Delete")]
-        public async Task<ActionResult> Delete(string ModelName, Guid Id, DpTypeEnum Type)
+        public async Task<ActionResult> Delete(SimpleDpModel dp)
         {
             DataPrivilegeVM vm = null;
-            if (Type == DpTypeEnum.User)
+            if (dp.Type == DpTypeEnum.User)
             {
-                vm = CreateVM<DataPrivilegeVM>(values: x => x.Entity.TableName == ModelName && x.Entity.UserId == Id && x.DpType == Type);
+                vm = CreateVM<DataPrivilegeVM>(values: x => x.Entity.TableName == dp.ModelName && x.Entity.UserId == dp.Id && x.DpType == dp.Type);
             }
             else
             {
-                vm = CreateVM<DataPrivilegeVM>(values: x => x.Entity.TableName == ModelName && x.Entity.GroupId == Id && x.DpType == Type);
+                vm = CreateVM<DataPrivilegeVM>(values: x => x.Entity.TableName == dp.ModelName && x.Entity.GroupId == dp.Id && x.DpType == dp.Type);
             }
             await vm.DoDeleteAsync();
             return Ok(1);
@@ -137,5 +137,12 @@ namespace WalkingTec.Mvvm.Admin.Api
             var rv = DC.Set<FrameworkGroup>().GetSelectListItems(LoginUserInfo.DataPrivileges, null, x => x.GroupName);
             return Ok(rv);
         }
+    }
+
+    public class SimpleDpModel
+    {
+        public string ModelName { get; set; }
+        public Guid Id { get; set; }
+        public DpTypeEnum Type { get; set; }
     }
 }
