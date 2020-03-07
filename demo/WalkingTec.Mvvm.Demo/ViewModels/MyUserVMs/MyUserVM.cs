2757bf4504b4c2f7968eda3389691de6e9bc4676
liuliang79@live.com <liuliang79@live.com>
2020-03-07 19:14:54 +0800

    fix: fix the code generator bug when there are multiple datacontext

--- demo/WalkingTec.Mvvm.Demo/ViewModels/MyUserVMs/MyUserVM.cs
+++ demo/WalkingTec.Mvvm.Demo/ViewModels/MyUserVMs/MyUserVM.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using WalkingTec.Mvvm.Core;
 using WalkingTec.Mvvm.Core.Extensions;
 using WalkingTec.Mvvm.Demo.Models;
-using Newtonsoft.Json;
-using WalkingTec.Mvvm.Mvc.Admin.ViewModels.FrameworkRoleVMs;
+
 
 namespace WalkingTec.Mvvm.Demo.ViewModels.MyUserVMs
 {
-    public class MyUserVM : BaseCRUDVM<MyUser>
+    public partial class MyUserVM : BaseCRUDVM<MyUser>
     {
         public List<ComboSelectListItem> AllUserRoless { get; set; }
         [Display(Name = "角色")]
@@ -18,26 +18,24 @@ namespace WalkingTec.Mvvm.Demo.ViewModels.MyUserVMs
         public List<ComboSelectListItem> AllUserGroupss { get; set; }
         [Display(Name = "用户组")]
         public List<Guid> SelectedUserGroupsIDs { get; set; }
-        [JsonIgnore]
-        public FrameworkRoleListVM RoleListVM { get; set; }
 
         public MyUserVM()
         {
             SetInclude(x => x.UserRoles);
             SetInclude(x => x.UserGroups);
-            RoleListVM = new FrameworkRoleListVM();
         }
 
         protected override void InitVM()
         {
-            AllUserRoless = DC.Set<FrameworkRole>().GetSelectListItems(LoginUserInfo.DataPrivileges, null, y => y.RoleName);
-            SelectedUserRolesIDs = Entity.UserRoles.Select(x => x.RoleId).ToList();
-            AllUserGroupss = DC.Set<FrameworkGroup>().GetSelectListItems(LoginUserInfo.DataPrivileges, null, y => y.GroupName);
-            SelectedUserGroupsIDs = Entity.UserGroups.Select(x => x.GroupId).ToList();
+            AllUserRoless = DC.Set<FrameworkRole>().GetSelectListItems(LoginUserInfo?.DataPrivileges, null, y => y.RoleName);
+            SelectedUserRolesIDs = Entity.UserRoles?.Select(x => x.RoleId).ToList();
+            AllUserGroupss = DC.Set<FrameworkGroup>().GetSelectListItems(LoginUserInfo?.DataPrivileges, null, y => y.GroupName);
+            SelectedUserGroupsIDs = Entity.UserGroups?.Select(x => x.GroupId).ToList();
         }
 
         public override void DoAdd()
         {
+            Entity.UserRoles = new List<FrameworkUserRole>();
             if (SelectedUserRolesIDs != null)
             {
                 foreach (var id in SelectedUserRolesIDs)
@@ -46,6 +44,7 @@ namespace WalkingTec.Mvvm.Demo.ViewModels.MyUserVMs
                 }
             }
 
+            Entity.UserGroups = new List<FrameworkUserGroup>();
             if (SelectedUserGroupsIDs != null)
             {
                 foreach (var id in SelectedUserGroupsIDs)
@@ -53,31 +52,21 @@ namespace WalkingTec.Mvvm.Demo.ViewModels.MyUserVMs
                     Entity.UserGroups.Add(new FrameworkUserGroup { GroupId = id });
                 }
             }
-            Entity.IsValid = true;
-            Entity.Password = Utils.GetMD5String(Entity.Password);
-
+           
             base.DoAdd();
         }
 
         public override void DoEdit(bool updateAllFields = false)
         {
-            if(SelectedUserRolesIDs == null || SelectedUserRolesIDs.Count == 0)
+            Entity.UserRoles = new List<FrameworkUserRole>();
+            if(SelectedUserRolesIDs != null )
             {
-                FC.Add("Entity.SelectedUserRolesIDs.DONOTUSECLEAR", "true");
-            }
-            else
-            {
-                Entity.UserRoles = new List<FrameworkUserRole>();
                 SelectedUserRolesIDs.ForEach(x => Entity.UserRoles.Add(new FrameworkUserRole { ID = Guid.NewGuid(), RoleId = x }));
             }
 
-            if(SelectedUserGroupsIDs == null || SelectedUserGroupsIDs.Count == 0)
+            Entity.UserGroups = new List<FrameworkUserGroup>();
+            if(SelectedUserGroupsIDs != null )
             {
-                FC.Add("Entity.SelectedUserGroupsIDs.DONOTUSECLEAR", "true");
-            }
-            else
-            {
-                Entity.UserGroups = new List<FrameworkUserGroup>();
                 SelectedUserGroupsIDs.ForEach(x => Entity.UserGroups.Add(new FrameworkUserGroup { ID = Guid.NewGuid(), GroupId = x }));
             }
 
