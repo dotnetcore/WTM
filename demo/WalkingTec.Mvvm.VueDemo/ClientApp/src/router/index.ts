b80a7dceada3db9407ecb5324ae8c3dd8a038e8e
liuliang79@live.com <liuliang79@live.com>
2020-04-01 22:13:52 +0800

    fix: update setupfile

--- demo/WalkingTec.Mvvm.VueDemo/ClientApp/src/router/index.ts
+++ demo/WalkingTec.Mvvm.VueDemo/ClientApp/src/router/index.ts
@@ -4,55 +4,52 @@ import Layout from "@/components/layout/index.vue";
 Vue.use(Router);
 
 export const constantRoutes: RouteConfig[] = [
-  {
-    path: "/",
-    component: Layout,
-    redirect: "/dashboard",
-    children: [
-      {
-        path: "dashboard",
-        component: () =>
-          import(
+    {
+        path: "/",
+        component: Layout,
+        redirect: "/dashboard",
+        children: [
+            {
+                path: "dashboard",
+                component: () =>
+                    import(
             /* webpackChunkName: "dashboard" */ "@/pages/dashboard/index.vue"
-          ),
-        name: "Dashboard",
-        meta: {
-          title: "dashboard",
-          icon: "el-icon-odometer",
-          affix: true
-        }
-      }
-    ]
-  }
-];
-
-export const asyncRoutes: RouteConfig[] = [
-  {
-    path: "/404",
-    component: () => import("@/pages/error-page/404.vue"),
-    meta: { hidden: true }
-  }
+                    ),
+                name: "Dashboard",
+                meta: {
+                    title: "dashboard",
+                    icon: "el-icon-odometer",
+                    affix: true
+                }
+            }
+        ]
+    },
+    {
+        path: "/404",
+        component: () => import("@/pages/error-page/404.vue"),
+        meta: { hidden: true }
+    }
 ];
 
 const createRouter = () =>
-  new Router({
-    // mode: 'history',
-    scrollBehavior: (to, from, savedPosition) => {
-      if (savedPosition) {
-        return savedPosition;
-      } else {
-        return { x: 0, y: 0 };
-      }
-    },
-    base: process.env.BASE_URL,
-    routes: constantRoutes
-  });
+    new Router({
+        // mode: 'history',
+        scrollBehavior: (to, from, savedPosition) => {
+            if (savedPosition) {
+                return savedPosition;
+            } else {
+                return { x: 0, y: 0 };
+            }
+        },
+        base: process.env.BASE_URL,
+        routes: constantRoutes
+    });
 
 const router = createRouter();
 
 export function resetRouter() {
-  const newRouter = createRouter();
-  (router as any).matcher = (newRouter as any).matcher; // reset router
+    const newRouter = createRouter();
+    (router as any).matcher = (newRouter as any).matcher; // reset router
 }
 
 export default router;
