50353447a65ebc568a01a199fa5604104ad1934d
LengYXin <lengyingxin8966@gmail.com>
2020-04-07 16:08:35 +0800

    test:vue update login pages

--- demo/WalkingTec.Mvvm.Next/ClientApp/packages/www-vue/src/views/pages/login/index.vue
+++ demo/WalkingTec.Mvvm.Next/ClientApp/packages/www-vue/src/views/pages/login/index.vue
@@ -1,49 +1,125 @@
 <template>
-  <div class="login-form">
-    <a-form :form="form" @submit="handleSubmit">
-      <a-form-item>
-        <a-input
-          v-decorator="[
-          'userName',
-          { rules: [{ required: true, message: 'Please input your username!' }],initialValue:'admin' },
-        ]"
-          placeholder="Username"
-        >
-          <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
-        </a-input>
-      </a-form-item>
-      <a-form-item>
-        <a-input
-          v-decorator="[
-          'password',
-          { rules: [{ required: true, message: 'Please input your Password!' }],initialValue:'000000' },
-        ]"
-          type="password"
-          placeholder="Password"
-        >
-          <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
-        </a-input>
-      </a-form-item>
-      <a-form-item>
-        <a-button
-          :loading="UserStore.Loading"
-          type="primary"
-          html-type="submit"
-          class="login-form-button"
-        >Log in</a-button>
-      </a-form-item>
-    </a-form>
-  </div>
+  <a-layout class="app-layout-login">
+    <a-layout class="app-login-back-1">
+      <a-layout-header>
+        <img class="app-login-logo" :src="imgs.logo" alt="" height="48" />
+      </a-layout-header>
+      <a-layout-content>
+        <div class="app-login-form">
+          <a-form :form="form" @submit="handleSubmit">
+            <h1 v-text="$GlobalConfig.settings.title"></h1>
+            <a-form-item>
+              <a-input
+                size="large"
+                v-decorator="[
+                  'userName',
+                  {
+                    rules: [
+                      { required: true, message: 'Please input your username!' }
+                    ],
+                    initialValue: 'admin'
+                  }
+                ]"
+                placeholder="Username"
+              >
+                <a-icon
+                  slot="prefix"
+                  type="user"
+                  style="color: rgba(0,0,0,.25)"
+                />
+              </a-input>
+            </a-form-item>
+            <a-form-item>
+              <a-input
+                size="large"
+                v-decorator="[
+                  'password',
+                  {
+                    rules: [
+                      { required: true, message: 'Please input your Password!' }
+                    ],
+                    initialValue: '000000'
+                  }
+                ]"
+                type="password"
+                placeholder="Password"
+              >
+                <a-icon
+                  slot="prefix"
+                  type="lock"
+                  style="color: rgba(0,0,0,.25)"
+                />
+              </a-input>
+            </a-form-item>
+            <a-form-item>
+              <a-button
+                :loading="UserStore.Loading"
+                type="primary"
+                html-type="submit"
+                block
+                size="large"
+                class="login-form-button"
+                >Log in</a-button
+              >
+            </a-form-item>
+          </a-form>
+        </div>
+      </a-layout-content>
+    </a-layout>
+    <a-layout-footer>
+      <a-row class=" app-login-links">
+        <a-col class="ant-typography" :lg="18" :md="24">
+          <h1>Quick Links</h1>
+          <a-row>
+            <a-col
+              v-for="value in links"
+              :key="value.name"
+              :lg="6"
+              :md="8"
+              :xs="8"
+              :sm="12"
+            >
+              <a :href="value.url" v-text="value.name" target="_blank"> </a>
+            </a-col>
+          </a-row>
+        </a-col>
+        <a-col :lg="6" :md="24">
+          <img :src="imgs.code" alt="" width="88" height="88" />
+        </a-col>
+      </a-row>
+      <div class="app-login-record">
+        <a href="https://wtmdoc.walkingtec.cn/" target="_blank"> Help</a> © 2019
+        WTM all rights reserved
+      </div>
+    </a-layout-footer>
+  </a-layout>
 </template>
 <script lang="ts">
 // import { observer } from "mobx-vue";
 import rootStore from "../../../rootStore";
 import { Component, Prop, Vue } from "vue-property-decorator";
+import code from "./code.png";
+import logo from "../../../assets/img/logo.png";
 // @observer
 @Component
 export default class Login extends Vue {
   UserStore = rootStore.UserStore;
   form;
+  imgs = {
+    code,
+    logo
+  };
+  links = [
+    { name: "GitHub", url: "https://github.com/dotnetcore/WTM" },
+    { name: "Vue", url: "https://cn.vuejs.org/" },
+    {
+      name: "Ant Design of Vue",
+      url: "https://www.antdv.com/docs/vue/introduce-cn/"
+    },
+    { name: "Rxjs", url: "https://rxjs.dev/" },
+    { name: "Mobx", url: "https://mobx.js.org/" },
+    { name: "Lodash", url: " https://lodash.com/" }
+  ];
   beforeCreate() {
     this.form = this.$form.createForm(this, {});
   }
@@ -65,20 +141,26 @@ export default class Login extends Vue {
   }
 }
 </script>
+<style lang="less">
+@import "./style.less";
+</style>
 <style scoped lang="less">
-.login-form {
+.app-login {
   min-height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  .ant-form {
-    width: 300px;
+}
+.app-login-form {
+  h1 {
+    font-size: 50px;
+    text-align: center;
+    margin-bottom: 20px;
   }
 }
-.login-form-forgot {
-  float: right;
-}
-.login-form-button {
-  width: 100%;
+.app-login-imgcode {
+  .ant-popover-message-title {
+    padding: 0;
+  }
+  .ant-popover-buttons {
+    display: none;
+  }
 }
 </style>
