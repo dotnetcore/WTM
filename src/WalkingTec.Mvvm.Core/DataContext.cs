d9f95b9397632e282f5e557f9d83ddacd85cb04e
liuliang79@live.com <liuliang79@live.com>
2020-04-13 10:27:29 +0800

    fix: fix unittest, and setup file

--- src/WalkingTec.Mvvm.Core/DataContext.cs
+++ src/WalkingTec.Mvvm.Core/DataContext.cs
@@ -349,7 +349,7 @@ namespace WalkingTec.Mvvm.Core
             }
             else
             {
-                return (IDataContext)this.GetType().GetConstructor(new Type[] { typeof(string), typeof(DBTypeEnum) }).Invoke(new object[] { CSName, DBType, Version });
+                return (IDataContext)this.GetType().GetConstructor(new Type[] { typeof(string), typeof(DBTypeEnum), typeof(string) }).Invoke(new object[] { CSName, DBType, Version });
             }
         }
 
@@ -361,7 +361,7 @@ namespace WalkingTec.Mvvm.Core
             }
             else
             {
-                return (IDataContext)this.GetType().GetConstructor(new Type[] { typeof(string), typeof(DBTypeEnum) }).Invoke(new object[] { CSName, DBType, Version });
+                return (IDataContext)this.GetType().GetConstructor(new Type[] { typeof(string), typeof(DBTypeEnum), typeof(string) }).Invoke(new object[] { CSName, DBType, Version });
             }
         }
         /// <summary>
