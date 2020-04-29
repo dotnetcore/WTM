00a9ab6c5474c19b5e29ef75b1721803fc936fd2
liuliang79@live.com <liuliang79@live.com>
2020-04-30 00:57:10 +0800

    fix: update unit test

--- test/WalkingTec.Mvvm.Admin.Test/FrameworkUserApiTest.cs
+++ test/WalkingTec.Mvvm.Admin.Test/FrameworkUserApiTest.cs
@@ -14,13 +14,13 @@ namespace WalkingTec.Mvvm.Admin.Test
     [TestClass]
     public class FrameworkUserApiTest
     {
-        private FrameworkUserController _controller;
+        private _FrameworkUserController _controller;
         private string _seed;
 
         public FrameworkUserApiTest()
         {
             _seed = Guid.NewGuid().ToString();
-            _controller = MockController.CreateApi<FrameworkUserController>(_seed, "user");
+            _controller = MockController.CreateApi<_FrameworkUserController>(_seed, "user");
         }
 
         [TestMethod]
