55b003e6956c313dccfaa7e5445dcc29df9fc105
LengYXin <lengyingxin8966@gmail.com>
2020-04-30 14:48:04 +0800

    test:add webpack-theme-color-replacer

--- demo/WalkingTec.Mvvm.Next/ClientApp/packages/www-vue/src/components/index.ts
+++ demo/WalkingTec.Mvvm.Next/ClientApp/packages/www-vue/src/components/index.ts
@@ -5,9 +5,12 @@ import grid from './grid/index.vue';
 import layout from './layout/index.vue';
 import actions from './pages/actions.vue';
 import filterForm from './pages/filterForm.vue';
+import { createFilterFormItem, createFormItem } from './pages/formItem';
 import modal from './pages/modal.vue';
 import avatar from './upload/avatar.vue';
-import { createFilterFormItem, createFormItem } from './pages/formItem';
+import themeColor from './layout/themeColor';
+console.log("themeColor", themeColor)
+// themeColor.getAntdSerials()
 export * from './pages/actions';
 function install(vue: typeof Vue) {
     directive.install(vue);
