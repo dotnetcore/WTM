a96dfd611ab9caf5d70068b22df403f4af239f35
zilong.hao <375755540@qq.com>
2020-05-17 18:28:49 +0800

    add 面包屑 多语言

--- demo/WalkingTec.Mvvm.VueDemo/ClientApp/src/components/layout/components/Card/index.vue
+++ demo/WalkingTec.Mvvm.VueDemo/ClientApp/src/components/layout/components/Card/index.vue
@@ -1,36 +1,50 @@
 /** * 卡片布局 */
 <template>
-  <article class="common-card">
-    <div v-if="$route.name" class="title">
-      {{ $route.name }}
-    </div>
-    <div class="content">
-      <slot />
-    </div>
-    <div v-if="$slots.footer" class="card-footer">
-      <slot name="footer" />
-    </div>
-  </article>
+    <article class="common-card">
+        <div v-if="$route.name" class="title">
+            {{ isDev ? $t("route." + $route.meta.key) : $route.meta.title }}
+        </div>
+        <div class="content">
+            <slot />
+        </div>
+        <div v-if="$slots.footer" class="card-footer">
+            <slot name="footer" />
+        </div>
+    </article>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import config from "@/config/index";
+
+@Component({
+    name: "card"
+})
+export default class extends Vue {
+    get isDev() {
+        return config.development;
+    }
+}
+</script>
 
 <style lang="less" scoped>
 .common-card {
-  border-top: 3px solid #409eff;
-  background-color: #ffffff;
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-  padding: 10px;
-  margin-bottom: 10px;
-  border-radius: 3px;
-  min-width: 300px;
-  .title {
-    font-size: 16px;
-    font-weight: 500;
+    border-top: 3px solid #409eff;
+    background-color: #ffffff;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+    padding: 10px;
     margin-bottom: 10px;
-  }
-  .card-footer {
-    border-top: 1px solid #f4f4f4;
-    text-align: center;
-    padding-top: 10px;
-  }
+    border-radius: 3px;
+    min-width: 300px;
+    .title {
+        font-size: 16px;
+        font-weight: 500;
+        margin-bottom: 10px;
+    }
+    .card-footer {
+        border-top: 1px solid #f4f4f4;
+        text-align: center;
+        padding-top: 10px;
+    }
 }
 </style>
