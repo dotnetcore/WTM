bd1c9f7b6a30c68c745827ff261baf01508cd0c5
Honglei Zhao <hi2015@sohu.com>
2020-05-20 13:53:39 +0800

    测试主子表

--- demo/WalkingTec.Mvvm.Demo/ViewModels/SchoolVMs/SchoolImportVM.cs
+++ demo/WalkingTec.Mvvm.Demo/ViewModels/SchoolVMs/SchoolImportVM.cs
@@ -15,26 +15,26 @@ namespace WalkingTec.Mvvm.Demo.ViewModels.SchoolVMs
         [Display(Name = "学校编码")]
         public ExcelPropety SchoolCode_Excel = ExcelPropety.CreateProperty<School>(x => x.SchoolCode);
 
-        [Display(Name = "学校类型")]
-        public ExcelPropety SchoolType_Excel = ExcelPropety.CreateProperty<School>(x => x.SchoolType);
-
         [Display(Name = "学校名称")]
         public ExcelPropety SchoolName_Excel = ExcelPropety.CreateProperty<School>(x => x.SchoolName);
+
+        [Display(Name = "学校类型")]
+        public ExcelPropety SchoolType_Excel = ExcelPropety.CreateProperty<School>(x => x.SchoolType);
 
         [Display(Name = "添加日期")]
         public ExcelPropety SchoolTime_Excel = ExcelPropety.CreateProperty<School>(x => x.CreateTime, true);
 
-        [Display(Name = "班级数量")]
-        public ExcelPropety ClassNumber_Excel = ExcelPropety.CreateProperty<School>(x => x.ClassNumber);
-
-        [Display(Name = "基础费用")]
-        public ExcelPropety SchoolMoney_Excel = ExcelPropety.CreateProperty<School>(x => x.SchoolMoney);
-
-        [Display(Name = "是否高级")]
-        public ExcelPropety IsHgih_Excel = ExcelPropety.CreateProperty<School>(x => x.IsHgih);
-
         [Display(Name = "备注")]
         public ExcelPropety Remark_Excel = ExcelPropety.CreateProperty<School>(x => x.Remark);
+
+        [Display(Name = "专业编码")]
+        public ExcelPropety MajorCode_Excel = ExcelPropety.CreateProperty<School>(x => x.Majors[0].MajorCode);
+
+        [Display(Name = "专业名称")]
+        public ExcelPropety MajorName_Excel = ExcelPropety.CreateProperty<School>(x => x.Majors[0].MajorName);
+
+        [Display(Name = "专业类型")]
+        public ExcelPropety MajorType_Excel = ExcelPropety.CreateProperty<School>(x => x.Majors[0].MajorType);
 
         protected override void InitVM()
         {
