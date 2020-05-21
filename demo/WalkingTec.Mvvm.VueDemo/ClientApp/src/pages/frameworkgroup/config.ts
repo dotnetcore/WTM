eb9d7b28612eb23e696e112c18dcc70fb245163a
zilong.hao <375755540@qq.com>
2020-05-22 01:01:09 +0800

    upd 多语言

--- demo/WalkingTec.Mvvm.VueDemo/ClientApp/src/pages/frameworkgroup/config.ts
+++ demo/WalkingTec.Mvvm.VueDemo/ClientApp/src/pages/frameworkgroup/config.ts
@@ -1,3 +1,4 @@
+import i18n from "@/lang";
 // 页面中，需要展示的动作按钮；增，改，删，导入，导出
 export const ASSEMBLIES: Array<string> = [
   "add",
@@ -8,8 +9,8 @@ export const ASSEMBLIES: Array<string> = [
 ];
 
 export const TABLE_HEADER: Array<object> = [
-  { key: "GroupCode", sortable: true, label: "用户组编码" },
-  { key: "GroupName", sortable: true, label: "用户组名称" },
-  { key: "GroupRemark", sortable: true, label: "备注" },
-  { isOperate: true, label: "操作", actions: ["detail", "edit", "deleted"] } //操作列
+  { key: "GroupCode", sortable: true, label: i18n.t(`frameworkgroup.PageName`) },
+  { key: "GroupName", sortable: true, label: i18n.t(`frameworkgroup.GroupName`) },
+  { key: "GroupRemark", sortable: true, label: i18n.t(`frameworkgroup.GroupRemark`) },
+  { isOperate: true, label: i18n.t(`table.actions`), actions: ["detail", "edit", "deleted"] } //操作列
 ];
