ab8c4515900682a2457c1a06fa9fb5d24e0e4582
liuliang79@live.com <liuliang79@live.com>
2020-07-05 17:32:24 +0800

    fix: update switch control, make switch the default control when the data type is bool in codegen fix: change UploadLimit in appsetting to long fix: change the default format of DateTime in ListVM to "yyyy-MM-dd"

--- demo/WalkingTec.Mvvm.Demo/ViewModels/StudentVMs/StudentSearcher.cs
+++ demo/WalkingTec.Mvvm.Demo/ViewModels/StudentVMs/StudentSearcher.cs
@@ -12,10 +12,14 @@ namespace WalkingTec.Mvvm.Demo.ViewModels.StudentVMs
 {
     public partial class StudentSearcher : BaseSearcher
     {
-        [Display(Name = "账号")]
-        public String ID { get; set; }
         [Display(Name = "姓名")]
         public String Name { get; set; }
+        [Display(Name = "性别")]
+        public SexEnum? Sex { get; set; }
+        [Display(Name = "邮编")]
+        public String ZipCode { get; set; }
+        [Display(Name = "是否有效")]
+        public Boolean? IsValid { get; set; }
         [Display(Name = "日期")]
         public DateRange EnRollDate { get; set; }
         public List<ComboSelectListItem> AllStudentMajors { get; set; }
