d33b9e0969bef9b2a810eea4062c694638322419
zilong.hao <375755540@qq.com>
2020-07-06 13:34:50 +0800

    upd 充满table

--- demo/WalkingTec.Mvvm.VueDemo/ClientApp/src/vue-custom/directive/el-height-full-auto-table/index.ts
+++ demo/WalkingTec.Mvvm.VueDemo/ClientApp/src/vue-custom/directive/el-height-full-auto-table/index.ts
@@ -6,32 +6,53 @@ import { DirectiveOptions } from "vue";
 import { debounce } from "@/util/throttle-debounce.ts";
 const PAUSE = 300;
 const BOTTOM_OFFSET = 68;
-const doResize = (el, binding, vnode) => {
-  const { componentInstance: $table } = vnode;
-  // if (!$table.height) {
-  //   throw new Error(`el-$table must set the height. Such as height='100px'`);
-  // }
-  if (!$table) return;
-  if (el.offsetTop !== 0) {
-    const height =
-      window.innerHeight - el.offsetTop - BOTTOM_OFFSET;
-    $table.layout.setHeight(height);
-    $table.doLayout();
+
+// todo update : 截流
+const getElementTop = (element) => {
+  let actualTop = element.offsetTop
+  let current = element.offsetParent
+  while (current !== null) {
+    actualTop += current.offsetTop
+    current = current.offsetParent
   }
+  return actualTop
+}
+
+
+const doResize = (el, vnode) => {
+  const { componentInstance: $table } = vnode;
+  if (!$table) return;
+  $table.$nextTick(() => {
+    const offsetTop = getElementTop(el);
+    const height =
+        window.innerHeight - offsetTop - BOTTOM_OFFSET;
+    $table.layout.setHeight(height - 2);
+    // $table.doLayout();
+  })
 };
+
 const debounceFn = debounce(doResize, PAUSE);
+
 const elHeightAdaptiveTable: DirectiveOptions = {
-  bind(el, binding, vnode) {
-    if (binding.value && binding.value !== 0) {
+  inserted: (el, { value }, vnode) => {
+    if (value) {
+        const { componentInstance: $table } = vnode;
+        if ($table) {
+          $table.layout.setHeight(value);
+        }
+    }
+  },
+  bind(el, { value }, vnode) {
+    if (value && value !== 0) {
       el.resizeListener = () => {
-        debounceFn(el, binding, vnode);
+        debounceFn(el, vnode);
       };
       window.addEventListener("resize", el.resizeListener);
     }
   },
-  update(el, binding, vnode) {
-    if (binding.value && binding.value !== 0) {
-      debounceFn(el, binding, vnode);
+  update(el, { value }, vnode) {
+    if (value && value !== 0) {
+      debounceFn(el, vnode);
     }
   },
   unbind(el) {
