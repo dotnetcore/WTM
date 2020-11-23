a616fe009872f1fc53e67923383503ad279c61ae
liu liang <liuliang79@live.com>
2020-11-24 00:59:26 +0800

    fix: change the id generate logic

--- src/WalkingTec.Mvvm.Mvc/Helper/FrameworkServiceExtension.cs
+++ src/WalkingTec.Mvvm.Mvc/Helper/FrameworkServiceExtension.cs
@@ -563,7 +563,7 @@ namespace WalkingTec.Mvvm.Mvc
                 {
                     var modelmenu = new FrameworkMenu
                     {
-                        //ID = Guid.NewGuid(),
+                        ID = Guid.NewGuid(),
                         PageName = area ?? localizer["DefaultArea"]
                     };
                     menus.Add(modelmenu);
