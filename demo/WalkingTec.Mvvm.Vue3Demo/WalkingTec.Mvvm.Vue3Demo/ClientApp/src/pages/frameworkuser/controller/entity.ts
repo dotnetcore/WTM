17be34fe82213ae156086c6d02b3da5de4975176
LengYXin <lengyingxin8966@gmail.com>
2021-04-09 17:31:18 +0800

    update

--- demo/WalkingTec.Mvvm.Vue3Demo/WalkingTec.Mvvm.Vue3Demo/ClientApp/src/pages/frameworkuser/controller/entity.ts
+++ demo/WalkingTec.Mvvm.Vue3Demo/WalkingTec.Mvvm.Vue3Demo/ClientApp/src/pages/frameworkuser/controller/entity.ts
@@ -6,12 +6,22 @@ import { EnumLocaleLabel } from '../locales';
  * 页面实体
  */
 class Entity {
+    /**
+     * 备注预留
+     * @type {WTM_EntitiesField}
+     * @memberof Entity
+     */
     readonly ID: WTM_EntitiesField = {
         // form 的 name 属性 解析为 Entity.ITCode
         name: ['Entity', 'ID'],
         // label 字段描述
         label: EnumLocaleLabel.ID,
     }
+    /**
+    * 备注预留
+    * @type {WTM_EntitiesField}
+    * @memberof Entity
+    */
     readonly ITCode: WTM_EntitiesField = {
         // form 的 name 属性 解析为 Entity.ITCode
         name: ['Entity', 'ITCode'],
@@ -22,37 +32,63 @@ class Entity {
         // 校验规则
         rules: [{ required: true, message: $i18n.toRulesMessage(EnumLocaleLabel.ITCode) }],
     }
+    /**
+    * 备注预留
+    * @type {WTM_EntitiesField}
+    * @memberof Entity
+    */
     readonly ITCode_Filter: WTM_EntitiesField = {
         name: 'ITCode',
         label: EnumLocaleLabel.ITCode,
     }
+    /**
+    * 备注预留
+    * @type {WTM_EntitiesField}
+    * @memberof Entity
+    */
     readonly Password: WTM_EntitiesField = {
         name: ['Entity', 'Password'],
         label: EnumLocaleLabel.Password,
-        // placeholder: $i18n.toPlaceholder(EnumLocaleLabel.Password),
         rules: [{ required: true, message: $i18n.toRulesMessage(EnumLocaleLabel.Password) }],
     }
+    /**
+    * 备注预留
+    * @type {WTM_EntitiesField}
+    * @memberof Entity
+    */
     readonly Email: WTM_EntitiesField = {
         name: ['Entity', 'Email'],
         label: EnumLocaleLabel.Email,
-        // placeholder: $i18n.toPlaceholder(EnumLocaleLabel.Email),
         valueType: WTM_ValueType.textarea,
         rules: [{ required: true, message: $i18n.toRulesMessage(EnumLocaleLabel.Email) }],
     }
+    /**
+    * 备注预留
+    * @type {WTM_EntitiesField}
+    * @memberof Entity
+    */
     readonly Name: WTM_EntitiesField = {
         name: ['Entity', 'Name'],
         label: EnumLocaleLabel.Name,
-        // placeholder: $i18n.toPlaceholder(EnumLocaleLabel.Name),
         rules: [{ required: true, message: $i18n.toRulesMessage(EnumLocaleLabel.Name) }],
     }
+    /**
+    * 备注预留
+    * @type {WTM_EntitiesField}
+    * @memberof Entity
+    */
     readonly Name_Filter: WTM_EntitiesField = {
         name: 'Name',
         label: EnumLocaleLabel.Name,
     }
+    /**
+    * 备注预留
+    * @type {WTM_EntitiesField}
+    * @memberof Entity
+    */
     readonly Gender: WTM_EntitiesField = {
         name: ['Entity', 'Gender'],
         label: EnumLocaleLabel.Sex,
-        // placeholder: $i18n.toPlaceholder(EnumLocaleLabel.Sex),
         valueType: WTM_ValueType.radio,
         // 远程数据
         request: async (formState) => {
@@ -63,18 +99,26 @@ class Entity {
         },
         rules: [{ required: true, message: $i18n.toRulesMessage(EnumLocaleLabel.Sex) }],
     }
+    /**
+    * 备注预留
+    * @type {WTM_EntitiesField}
+    * @memberof Entity
+    */
     readonly SelectedRolesCodes: WTM_EntitiesField = {
         name: 'SelectedRolesCodes',
         label: EnumLocaleLabel.RoleName,
-        // placeholder: $i18n.toPlaceholder(EnumLocaleLabel.RoleName),
         valueType: WTM_ValueType.checkbox,
         request: async () => FieldRequest('/api/_FrameworkUserBase/GetFrameworkRoles'),
         rules: [{ required: true, message: $i18n.toRulesMessage(EnumLocaleLabel.RoleName), type: "array" }],
     }
+    /**
+    * 备注预留
+    * @type {WTM_EntitiesField}
+    * @memberof Entity
+    */
     readonly SelectedGroupCodes: WTM_EntitiesField = {
         name: 'SelectedGroupCodes',
         label: EnumLocaleLabel.GroupName,
-        // placeholder: $i18n.toPlaceholder(EnumLocaleLabel.GroupName),
         valueType: WTM_ValueType.checkbox,
         request: async () => FieldRequest('/api/_FrameworkUserBase/GetFrameworkGroups'),
         rules: [{ required: true, message: $i18n.toRulesMessage(EnumLocaleLabel.GroupName), type: "array" }],
