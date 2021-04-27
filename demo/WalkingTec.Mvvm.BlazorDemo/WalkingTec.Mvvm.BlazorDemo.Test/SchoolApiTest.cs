bd92f2f63fc5cc3657ab5e1eb8dee71808484c7c
liu liang <liuliang79@live.com>
2021-04-27 22:30:32 +0800

    update blazor

---
+++ demo/WalkingTec.Mvvm.BlazorDemo/WalkingTec.Mvvm.BlazorDemo.Test/SchoolApiTest.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WalkingTec.Mvvm.Core;
+using WalkingTec.Mvvm.BlazorDemo.Controllers;
+using WalkingTec.Mvvm.BlazorDemo.ViewModel.SchoolVMs;
+using WalkingTec.Mvvm.Demo.Models;
+using WalkingTec.Mvvm.BlazorDemo.DataAccess;
+
+
+namespace WalkingTec.Mvvm.BlazorDemo.Test
+{
+    [TestClass]
+    public class SchoolApiTest
+    {
+        private SchoolController _controller;
+        private string _seed;
+
+        public SchoolApiTest()
+        {
+            _seed = Guid.NewGuid().ToString();
+            _controller = MockController.CreateApi<SchoolController>(new DataContext(_seed, DBTypeEnum.Memory), "user");
+        }
+
+        [TestMethod]
+        public void SearchTest()
+        {
+            ContentResult rv = _controller.Search(new SchoolSearcher()) as ContentResult;
+            Assert.IsTrue(string.IsNullOrEmpty(rv.Content)==false);
+        }
+
+        [TestMethod]
+        public void CreateTest()
+        {
+            SchoolVM vm = _controller.Wtm.CreateVM<SchoolVM>();
+            School v = new School();
+            
+            v.ID = 78;
+            v.SchoolCode = "HTw";
+            v.SchoolName = "e3Q";
+            v.SchoolType = WalkingTec.Mvvm.Demo.Models.SchoolTypeEnum.PUB;
+            v.Remark = "N1afKxyPA";
+            vm.Entity = v;
+            var rv = _controller.Add(vm);
+            Assert.IsInstanceOfType(rv, typeof(OkObjectResult));
+
+            using (var context = new DataContext(_seed, DBTypeEnum.Memory))
+            {
+                var data = context.Set<School>().Find(v.ID);
+                
+                Assert.AreEqual(data.ID, 78);
+                Assert.AreEqual(data.SchoolCode, "HTw");
+                Assert.AreEqual(data.SchoolName, "e3Q");
+                Assert.AreEqual(data.SchoolType, WalkingTec.Mvvm.Demo.Models.SchoolTypeEnum.PUB);
+                Assert.AreEqual(data.Remark, "N1afKxyPA");
+                Assert.AreEqual(data.CreateBy, "user");
+                Assert.IsTrue(DateTime.Now.Subtract(data.CreateTime.Value).Seconds < 10);
+            }
+        }
+
+        [TestMethod]
+        public void EditTest()
+        {
+            School v = new School();
+            using (var context = new DataContext(_seed, DBTypeEnum.Memory))
+            {
+       			
+                v.ID = 78;
+                v.SchoolCode = "HTw";
+                v.SchoolName = "e3Q";
+                v.SchoolType = WalkingTec.Mvvm.Demo.Models.SchoolTypeEnum.PUB;
+                v.Remark = "N1afKxyPA";
+                context.Set<School>().Add(v);
+                context.SaveChanges();
+            }
+
+            SchoolVM vm = _controller.Wtm.CreateVM<SchoolVM>();
+            var oldID = v.ID;
+            v = new School();
+            v.ID = oldID;
+       		
+            v.SchoolCode = "yEc5BG4";
+            v.SchoolName = "h8ZbNHI";
+            v.SchoolType = WalkingTec.Mvvm.Demo.Models.SchoolTypeEnum.PUB;
+            v.Remark = "O91";
+            vm.Entity = v;
+            vm.FC = new Dictionary<string, object>();
+			
+            vm.FC.Add("Entity.ID", "");
+            vm.FC.Add("Entity.SchoolCode", "");
+            vm.FC.Add("Entity.SchoolName", "");
+            vm.FC.Add("Entity.SchoolType", "");
+            vm.FC.Add("Entity.Remark", "");
+            var rv = _controller.Edit(vm);
+            Assert.IsInstanceOfType(rv, typeof(OkObjectResult));
+
+            using (var context = new DataContext(_seed, DBTypeEnum.Memory))
+            {
+                var data = context.Set<School>().Find(v.ID);
+ 				
+                Assert.AreEqual(data.SchoolCode, "yEc5BG4");
+                Assert.AreEqual(data.SchoolName, "h8ZbNHI");
+                Assert.AreEqual(data.SchoolType, WalkingTec.Mvvm.Demo.Models.SchoolTypeEnum.PUB);
+                Assert.AreEqual(data.Remark, "O91");
+                Assert.AreEqual(data.UpdateBy, "user");
+                Assert.IsTrue(DateTime.Now.Subtract(data.UpdateTime.Value).Seconds < 10);
+            }
+
+        }
+
+		[TestMethod]
+        public void GetTest()
+        {
+            School v = new School();
+            using (var context = new DataContext(_seed, DBTypeEnum.Memory))
+            {
+        		
+                v.ID = 78;
+                v.SchoolCode = "HTw";
+                v.SchoolName = "e3Q";
+                v.SchoolType = WalkingTec.Mvvm.Demo.Models.SchoolTypeEnum.PUB;
+                v.Remark = "N1afKxyPA";
+                context.Set<School>().Add(v);
+                context.SaveChanges();
+            }
+            var rv = _controller.Get(v.ID.ToString());
+            Assert.IsNotNull(rv);
+        }
+
+        [TestMethod]
+        public void BatchDeleteTest()
+        {
+            School v1 = new School();
+            School v2 = new School();
+            using (var context = new DataContext(_seed, DBTypeEnum.Memory))
+            {
+				
+                v1.ID = 78;
+                v1.SchoolCode = "HTw";
+                v1.SchoolName = "e3Q";
+                v1.SchoolType = WalkingTec.Mvvm.Demo.Models.SchoolTypeEnum.PUB;
+                v1.Remark = "N1afKxyPA";
+                v2.ID = 65;
+                v2.SchoolCode = "yEc5BG4";
+                v2.SchoolName = "h8ZbNHI";
+                v2.SchoolType = WalkingTec.Mvvm.Demo.Models.SchoolTypeEnum.PUB;
+                v2.Remark = "O91";
+                context.Set<School>().Add(v1);
+                context.Set<School>().Add(v2);
+                context.SaveChanges();
+            }
+
+            var rv = _controller.BatchDelete(new string[] { v1.ID.ToString(), v2.ID.ToString() });
+            Assert.IsInstanceOfType(rv, typeof(OkObjectResult));
+
+            using (var context = new DataContext(_seed, DBTypeEnum.Memory))
+            {
+                var data1 = context.Set<School>().Find(v1.ID);
+                var data2 = context.Set<School>().Find(v2.ID);
+                Assert.AreEqual(data1, null);
+            Assert.AreEqual(data2, null);
+            }
+
+            rv = _controller.BatchDelete(new string[] {});
+            Assert.IsInstanceOfType(rv, typeof(OkResult));
+
+        }
+
+
+    }
+}
---
+++ demo/WalkingTec.Mvvm.BlazorDemo/WalkingTec.Mvvm.BlazorDemo/Controllers/SchoolController.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WalkingTec.Mvvm.Core;
+using WalkingTec.Mvvm.Core.Extensions;
+using WalkingTec.Mvvm.Mvc;
+using WalkingTec.Mvvm.BlazorDemo.ViewModel.SchoolVMs;
+using WalkingTec.Mvvm.Demo.Models;
+
+
+namespace WalkingTec.Mvvm.BlazorDemo.Controllers
+{
+    
+    [AuthorizeJwtWithCookie]
+    [ActionDescription("学校管理")]
+    [ApiController]
+    [Route("api/School")]
+	public partial class SchoolController : BaseApiController
+    {
+        [ActionDescription("Sys.Search")]
+        [HttpPost("Search")]
+		public IActionResult Search(SchoolSearcher searcher)
+        {
+            if (ModelState.IsValid)
+            {
+                var vm = Wtm.CreateVM<SchoolListVM>();
+                vm.Searcher = searcher;
+                return Content(vm.GetJson(enumToString: false));
+            }
+            else
+            {
+                return BadRequest(ModelState.GetErrorJson());
+            }
+        }
+
+        [ActionDescription("Sys.Get")]
+        [HttpGet("{id}")]
+        public SchoolVM Get(string id)
+        {
+            var vm = Wtm.CreateVM<SchoolVM>(id);
+            return vm;
+        }
+
+        [ActionDescription("Sys.Create")]
+        [HttpPost("Add")]
+        public IActionResult Add(SchoolVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.GetErrorJson());
+            }
+            else
+            {
+                vm.DoAdd();
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState.GetErrorJson());
+                }
+                else
+                {
+                    return Ok(vm.Entity);
+                }
+            }
+
+        }
+
+        [ActionDescription("Sys.Edit")]
+        [HttpPut("Edit")]
+        public IActionResult Edit(SchoolVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.GetErrorJson());
+            }
+            else
+            {
+                vm.DoEdit(false);
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState.GetErrorJson());
+                }
+                else
+                {
+                    return Ok(vm.Entity);
+                }
+            }
+        }
+
+		[HttpPost("BatchDelete")]
+        [ActionDescription("Sys.Delete")]
+        public IActionResult BatchDelete(string[] ids)
+        {
+            var vm = Wtm.CreateVM<SchoolBatchVM>();
+            if (ids != null && ids.Count() > 0)
+            {
+                vm.Ids = ids;
+            }
+            else
+            {
+                return Ok();
+            }
+            if (!ModelState.IsValid || !vm.DoBatchDelete())
+            {
+                return BadRequest(ModelState.GetErrorJson());
+            }
+            else
+            {
+                return Ok(ids.Count());
+            }
+        }
+
+
+        [ActionDescription("Sys.Export")]
+        [HttpPost("ExportExcel")]
+        public IActionResult ExportExcel(SchoolSearcher searcher)
+        {
+            var vm = Wtm.CreateVM<SchoolListVM>();
+            vm.Searcher = searcher;
+            vm.SearcherMode = ListVMSearchModeEnum.Export;
+            return vm.GetExportData();
+        }
+
+        [ActionDescription("Sys.CheckExport")]
+        [HttpPost("ExportExcelByIds")]
+        public IActionResult ExportExcelByIds(string[] ids)
+        {
+            var vm = Wtm.CreateVM<SchoolListVM>();
+            if (ids != null && ids.Count() > 0)
+            {
+                vm.Ids = new List<string>(ids);
+                vm.SearcherMode = ListVMSearchModeEnum.CheckExport;
+            }
+            return vm.GetExportData();
+        }
+
+        [ActionDescription("Sys.DownloadTemplate")]
+        [HttpGet("GetExcelTemplate")]
+        public IActionResult GetExcelTemplate()
+        {
+            var vm = Wtm.CreateVM<SchoolImportVM>();
+            var qs = new Dictionary<string, string>();
+            foreach (var item in Request.Query.Keys)
+            {
+                qs.Add(item, Request.Query[item]);
+            }
+            vm.SetParms(qs);
+            var data = vm.GenerateTemplate(out string fileName);
+            return File(data, "application/vnd.ms-excel", fileName);
+        }
+
+        [ActionDescription("Sys.Import")]
+        [HttpPost("Import")]
+        public ActionResult Import(SchoolImportVM vm)
+        {
+
+            if (vm.ErrorListVM.EntityList.Count > 0 || !vm.BatchSaveData())
+            {
+                return BadRequest(vm.GetErrorJson());
+            }
+            else
+            {
+                return Ok(vm.EntityList.Count);
+            }
+        }
+
+
+    }
+}
