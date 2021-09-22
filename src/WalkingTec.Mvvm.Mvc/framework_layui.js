2bffea20dae393c540223b5299cbfebaa13bf2a9
Rea Zhuang <reazhuang@outlook.com>
2021-09-22 19:50:54 +0800

    修复在图表在内核为75版本的浏览器无法显示的问题 (#512)
    低版本浏览器不支持replaceAll，改用replace

--- src/WalkingTec.Mvvm.Mvc/framework_layui.js
+++ src/WalkingTec.Mvvm.Mvc/framework_layui.js
@@ -1014,7 +1014,7 @@ window.ff = {
     RefreshChart: function (chartid) {
         var postdata = '';
 
-        var searcher = $('form[chartlink*="' + chartid + '"]')
+        var searcher = $('form[chartlink*="' + chartid + '"]');
         if (searcher !== undefined && searcher.length > 0) {
             postdata = ff.GetFormData(searcher[0].id);
         }
@@ -1026,7 +1026,7 @@ window.ff = {
                 async: true,
                 success: function (data, textStatus, request) {
                     if (data.series != undefined) {
-                        data.series = data.series.replaceAll('"type":"charttype"', eval(chartid+'ChartType'));
+                        data.series = data.series.replace('"type":"charttype"', eval(chartid+'ChartType'));
                     }
                     eval(chartid + 'Chart.setOption({dataset: JSON.parse(data.dataset),series: JSONfns.parse(data.series)},{replaceMerge:\'series\'});');
                     if (eval(chartid + 'ChartLegend') == 'true') {
