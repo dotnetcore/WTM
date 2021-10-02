65ce200da3332b31c08de12bf518b29824377d56
Rea Zhuang <reazhuang@outlook.com>
2021-10-02 16:45:41 +0800

    Update BasePagedListVM.cs (#517)
    让AddErrorColumn()增加的错误信息列，固定在表格的右侧，不然表格列数太多时，错误信息被推到屏幕外，用户有一种提交失败但不提示的感觉

--- src/WalkingTec.Mvvm.Core/BasePagedListVM.cs
+++ src/WalkingTec.Mvvm.Core/BasePagedListVM.cs
@@ -1011,11 +1011,11 @@ namespace WalkingTec.Mvvm.Core
                 var temp = GridHeaders as List<GridColumn<TModel>>;
                 if (temp.Where(x => x.ColumnType == GridColumnTypeEnum.Action).FirstOrDefault() == null)
                 {
-                    temp.Add(this.MakeGridColumn(x => x.BatchError, Width: 200, Header: Core.CoreProgram._localizer?["Sys.Error"]).SetForeGroundFunc(x => "ff0000"));
+                    temp.Add(this.MakeGridColumn(x => x.BatchError, Width: 200, Header: Core.CoreProgram._localizer?["Sys.Error"]).SetForeGroundFunc(x => "ff0000").SetFixed(GridColumnFixedEnum.Right));
                 }
                 else
                 {
-                    temp.Insert(temp.Count - 1, this.MakeGridColumn(x => x.BatchError, Width: 200, Header: Core.CoreProgram._localizer?["Sys.Error"]).SetForeGroundFunc(x => "ff0000"));
+                    temp.Insert(temp.Count - 1, this.MakeGridColumn(x => x.BatchError, Width: 200, Header: Core.CoreProgram._localizer?["Sys.Error"]).SetForeGroundFunc(x => "ff0000").SetFixed(GridColumnFixedEnum.Right));
                 }
             }
         }
