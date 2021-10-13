f0eae6990e74f190fc60cfbe242b1a0fc8818b34
cai <390426004@qq.com>
2021-10-13 10:12:40 +0800

    [fix] mutiselect

--- demo/WalkingTec.Mvvm.Vue3Demo/ClientApp/src/components/page/grid/defaultOptions.ts
+++ demo/WalkingTec.Mvvm.Vue3Demo/ClientApp/src/components/page/grid/defaultOptions.ts
@@ -10,6 +10,8 @@ export default (i18n: VueI18n) => {
         loadingOverlayComponent: frameworkComponents.loadingOverlay,
         noRowsOverlayComponent: frameworkComponents.noRowsOverlay,
         localeText: i18n.locale === 'zh' ? AG_GRID_LOCALE_ZH : undefined,
+        rowSelection:"multiple",
+        rowMultiSelectWithClick:true,
         // debug:true,
         defaultColDef: {
             sortable: true,
