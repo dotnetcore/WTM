18ecf14118efbc0023ae42e65942a672680703db
liu liang <liuliang79@live.com>
2021-10-17 18:20:54 +0800

    update vue3api

--- demo/WalkingTec.Mvvm.Vue3Demo/Areas/_Admin/ViewModels/FrameworkUserVms/FrameworkUserBatchVM.cs
+++ demo/WalkingTec.Mvvm.Vue3Demo/Areas/_Admin/ViewModels/FrameworkUserVms/FrameworkUserBatchVM.cs
@@ -1,7 +1,11 @@
 // WTM默认页面 Wtm buidin page
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using WalkingTec.Mvvm.Core;
+using WalkingTec.Mvvm.Core.Extensions;
 
 namespace WalkingTec.Mvvm.Mvc.Admin.ViewModels.FrameworkUserVms
 {
@@ -12,11 +16,39 @@ namespace WalkingTec.Mvvm.Mvc.Admin.ViewModels.FrameworkUserVms
             ListVM = new FrameworkUserListVM();
             LinkedVM = new FrameworkUser_BatchEdit();
         }
+        public override bool DoBatchEdit()
+        {
+            var entityList = DC.Set<FrameworkUser>().AsNoTracking().CheckIDs(Ids.ToList()).ToList();
+            foreach (var entity in entityList)
+            {
+                List<Guid> todelete = new List<Guid>();
+                todelete.AddRange(DC.Set<FrameworkUserRole>().AsNoTracking().Where(x => x.UserCode == entity.ITCode).Select(x => x.ID));
+                foreach (var item in todelete)
+                {
+                    DC.DeleteEntity(new FrameworkUserRole { ID = item });
+                }
+
+                if (LinkedVM.SelectedRolesCodes != null)
+                {
+                    foreach (var rolecode in LinkedVM.SelectedRolesCodes)
+                    {
+                        FrameworkUserRole r = new FrameworkUserRole
+                        {
+                            RoleCode = rolecode,
+                            UserCode = entity.ITCode
+                        };
+                        DC.AddEntity(r);
+                    }
+                }
+            }
+            return base.DoBatchEdit();
+        }
 
     }
 
     public class FrameworkUser_BatchEdit : BaseVM
     {
+        public List<string> SelectedRolesCodes { get; set; }
 
     }
 
