534c858b0a68129a11631a5ae154d01cd7177789
xuyingxuying <39507855+xuyingxuying@users.noreply.github.com>
2021-11-11 11:54:48 +0800

    Update login.vue

--- demo/WalkingTec.Mvvm.Vue3Demo/ClientApp/src/layouts/login.vue
+++ demo/WalkingTec.Mvvm.Vue3Demo/ClientApp/src/layouts/login.vue
@@ -2,7 +2,7 @@
   <a-layout class="w-login" :class="contentClass">
     <a-layout-header class="w-login-header">
       <img :src="logo" class="w-login-logo" />
-      <h1 class="fa-toggle-on fa" style="color:red;"></h1>
+      <h1 class="fa-toggle-on fa fa-lg" style="color:red;"></h1>
     </a-layout-header>
     <a-layout-content class="w-login-content">
       <a-form
