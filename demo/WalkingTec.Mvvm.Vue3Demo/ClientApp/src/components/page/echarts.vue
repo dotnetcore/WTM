141396875528a3509b591affe0b4eee25e8e644f
cai <390426004@qq.com>
2021-11-14 18:56:46 +0800

    [fix] vue chart temp

--- demo/WalkingTec.Mvvm.Vue3Demo/ClientApp/src/components/page/echarts.vue
+++ demo/WalkingTec.Mvvm.Vue3Demo/ClientApp/src/components/page/echarts.vue
@@ -10,64 +10,69 @@ import "echarts";
 import VChart, { THEME_KEY } from "vue-echarts";
 import { Options, Prop, Provide, Vue } from "vue-property-decorator";
 // 文档地址 https://echarts.apache.org/handbook/zh/basics/import
-@Options({ components: { VChart } })
-export default class extends Vue {
-  // 图表参数
-  @Prop({ type: Object }) option;
-  // 需要替换的 key  {charttype:'pie'}
-  @Prop({ type: Object }) replace;
-  @Prop({ type: Object }) type;
-  @Prop({ type: Object }) title;
-    @Prop({ type: Boolean }) showtooltip;
-    @Prop({ type: Boolean }) ishorizontal;
-  //   api 请求参数 string | AjaxRequest
-  @Prop({}) request;
-  @Provide() [THEME_KEY]: "dark";
-  get _option() {
-    return this.lodash.merge(
-      {},
-      this.requestOption,
-      this.option,
-      this.baseOption
-    );
-  }
-  spinning = true;
-  requestOption = {};
-  baseOption = {};
-    async onRequest() {
-        //this.replace = "{ charttype: '" + this.type + "' }";
-        if (this.request) {
-            try {
-                const res = await this.$Ajax.post<any>(this.request);
-                const option = this.lodash.mapValues<any, any>(res, val => {
-                    if (this.lodash.isString(val)) {
-                        // 替换所有配置的数据
-                        this.lodash.mapValues(this.replace, (regStr, key) => {
-                            const reg = new RegExp(key, "g");
-                            val = val.replace(reg, regStr);
-                        });
-                        return JSON.parse(val);
-                    }
-                });
-                this.requestOption = option;
-            } catch (error) {
-                console.log(
-                    "🚀 ~ file: echarts.vue ~ line 60 ~ extends ~ onRequest ~ error",
-                    error
-                );
-            }
+    @Options({ components: { VChart } })
+    export default class extends Vue {
+        // 图表参数
+        @Prop({ type: Object }) option;
+        // 需要替换的 key  {charttype:'pie'}
+        @Prop({ type: Object }) replace;
+        @Prop({ type: Object }) type;
+        @Prop({ type: Object }) title;
+        @Prop({ type: Object, default: 'X' }) namex;
+        @Prop({ type: Object, default: 'Y' }) namey;
+
+        @Prop({ type: Boolean, default: false }) opensmooth;
+        @Prop({ type: Boolean, default: true }) showlegent;
+        @Prop({ type: Boolean, default: true }) showtooltip;
+        @Prop({ type: Boolean, default: false }) ishorizontal;
+        //   api 请求参数 string | AjaxRequest
+        @Prop({}) request;
+        @Provide() [THEME_KEY]: "dark";
+        get _option() {
+            return this.lodash.merge(
+                {},
+                this.requestOption,
+                this.option,
+                this.baseOption
+            );
         }
-        this.spinning = false;
-    }
-    setBase() {
-        var tooltip = "";
-        if (this.showtooltip == true) {
-            tooltip = "tooltip: {},";
-            if (this.type == "scatter") {
-                tooltip = `tooltip:{
+        spinning = true;
+        requestOption = {};
+        baseOption = {};
+        async onRequest() {
+            //this.replace = "{ charttype: '" + this.type + "' }";
+            if (this.request) {
+                try {
+                    const res = await this.$Ajax.post<any>(this.request);
+                    const option = this.lodash.mapValues<any, any>(res, val => {
+                        if (this.lodash.isString(val)) {
+                            // 替换所有配置的数据
+                            this.lodash.mapValues(this.replace, (regStr, key) => {
+                                const reg = new RegExp(key, '"type":"charttype"');
+                                val = val.replace(reg, regStr);
+                            });
+                            return JSON.parse(val);
+                        }
+                    });
+                    this.requestOption = option;
+                } catch (error) {
+                    console.log(
+                        "🚀 ~ file: echarts.vue ~ line 60 ~ extends ~ onRequest ~ error",
+                        error
+                    );
+                }
+            }
+
+        }
+        setBase() {
+            var tooltip = "";
+            if (this.showtooltip == true) {
+                tooltip = "tooltip: {},";
+                if (this.type == "scatter") {
+                    tooltip = `tooltip:{
                     formatter: function (params) {
-                            var xl = 'x';
-                            var yl = 'y';
+                            var xl = '${this.namex}';
+                            var yl = '${this.namey}';
                             var al = '附加';
                             var cl = '';
                             return params.seriesName + ' <br/>'
@@ -76,52 +81,65 @@ export default class extends Vue {
                                 + al + params.value[2] + ' <br/>'
                                 + cl + params.value[3] + ' <br/>';
                         },}, `;
+                }
+                if (this.type == "line") {
+                    tooltip = "tooltip: {trigger: 'axis'},";
+                }
             }
-            if (this.type == "line") {
-                tooltip = "tooltip: {trigger: 'axis'},";
+            var legend = "";
+            if (this.showlegent == false) {
+                legend = `legend: {show: false},`;
             }
-        }
-        var xAxis = "",
-            yAxis = "";
-        if (this.type != "pie" && this.type != "piehollow") {
-            if (this.ishorizontal == false) {
-                xAxis = `xAxis: {name:'X',type: 'category'},`;
-                yAxis = `yAxis: {name:'Y'},`;
-            }
-            else {
-                xAxis = `xAxis: {name:'Y'},`;
-                yAxis = `yAxis: {name:'X',type: 'category'},`;
+            //if (this.type == 'piehollow')
+            //    this.replace = `"type":"pie","radius": ["40%", "70%"]`;
+            //else
+            //    this.replace = `"type":"${this.type}"`;
+            //if (this.type == 'line')
+            //    this.replace += `,"smooth": ${this.opensmooth}`;
+
+            var xAxis = "";
+            var yAxis = "";
+            if (this.type != "pie" && this.type != "piehollow") {
+                if (this.ishorizontal == false) {
+                    xAxis = `xAxis: {name:'${this.namex}',type: 'category'},`;
+                    yAxis = `yAxis: {name:'${this.namey}'},`;
+                }
+                else {
+                    xAxis = `xAxis: {name:'${this.namey}'},`;
+                    yAxis = `yAxis: {name:'${this.namex}',type: 'category'},`;
+                }
+
+                if (this.type == "scatter") {
+                    xAxis = `xAxis: {{ name:'${this.namex}',type: 'value',splitLine: {{ lineStyle: {{ type: 'dashed'}} }} }},`;
+                    yAxis = `yAxis:{{name:'${this.namey}',splitLine:{{lineStyle:{{type: 'dashed'}} }},scale: true}},`;
+                }
             }
 
-            if (this.type == "scatter") {
-                xAxis = `xAxis: {{ name:'X',type: 'value',splitLine: {{ lineStyle: {{ type: 'dashed'}} }} }},`;
-                yAxis = `yAxis:{{name:'Y',splitLine:{{lineStyle:{{type: 'dashed'}} }},scale: true}},`;
+            var opTitle = '';
+            if (this.title != undefined) {
+                opTitle = `title:{text: '${this.title}'},`
             }
-        }
-
-        var str = `{
+            var str = `{
+    ${opTitle}
+    ${legend}
     ${tooltip}
     ${xAxis}
     ${yAxis}
                 }`;
-        this.baseOption = eval("(" + str + ")");
-    }
+            this.baseOption = eval("(" + str + ")");
+        }
 
-  created() {
-    this.onRequest();
-    this.setBase();
-  }
-  mounted() {}
-}
+        created() {
+            this.setBase();
+            this.onRequest();
+
+            this.spinning = false;
+        }
+        mounted() { }
+    }
 </script>
 <style lang="less">
-    .app-chart-spin {
-        width: 100%;
-        height: 100%;
-        .ant-spin-nested-loading, .ant-spin-container
-    {
-        width: 100%;
-        height: 100%;
-    }
+    .app-chart-spin { width: 100%; height: 100%; .ant-spin-nested-loading, .ant-spin-container
+    { width: 100%; height: 100%; }
     }
 </style>
