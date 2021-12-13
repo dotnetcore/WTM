801812091e62f9de109c9a621ec0e8ab84857fbe
liu liang <liuliang79@live.com>
2021-12-13 22:41:09 +0800

    update

--- demo/WalkingTec.Mvvm.Vue3Demo/ClientApp/src/router.ts
+++ demo/WalkingTec.Mvvm.Vue3Demo/ClientApp/src/router.ts
@@ -153,7 +153,6 @@ class AppRouter {
   async onGetRequest(): Promise<Array<{ label: string, value: string } & RouteRecordRaw>> {
 
       return lodash.map(lodash.filter(this.RouterFiles, 'name'), item => {
-          debugger;
         var a = lodash.assign({ label: $i18n.t(`PageName.${lodash.get(item, 'name')}`), value: lodash.get(item, 'component.name') }, item)
         return a;
     })
