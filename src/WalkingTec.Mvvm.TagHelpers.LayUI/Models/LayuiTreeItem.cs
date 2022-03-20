029896d757fc9deef6dfeed1d3043c795ba1c655
liu liang <liuliang79@live.com>
2022-03-21 02:46:43 +0800

    fix treecontainer

--- src/WalkingTec.Mvvm.TagHelpers.LayUI/Models/LayuiTreeItem.cs
+++ src/WalkingTec.Mvvm.TagHelpers.LayUI/Models/LayuiTreeItem.cs
@@ -34,4 +34,29 @@ namespace WalkingTec.Mvvm.TagHelpers.LayUI
         [JsonPropertyName("icon")]
         public string Icon { get; set; }
     }
+
+    public class LayuiTreeItem2
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("children")]
+        public List<LayuiTreeItem2> Children { get; set; }
+
+        [JsonPropertyName("href")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("spread")]
+        public bool Expand { get; set; }
+
+        [JsonPropertyName("checked")]
+        public bool Checked { get; set; }
+
+        [JsonPropertyName("level")]
+        public int Level { get; set; }
+    }
+
 }
