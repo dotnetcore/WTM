0aa8c841f93d0eebc466549c9bc6440941a776ef
liu liang <liuliang79@live.com>
2022-07-25 22:32:54 +0800

    update tenent url check

--- src/WalkingTec.Mvvm.Mvc/Helper/FrameworkServiceExtension.cs
+++ src/WalkingTec.Mvvm.Mvc/Helper/FrameworkServiceExtension.cs
@@ -46,6 +46,7 @@ using WalkingTec.Mvvm.TagHelpers.LayUI;
 using Microsoft.AspNetCore.SpaServices.Extensions;
 using Microsoft.Extensions.FileProviders;
 using WalkingTec.Mvvm.Core.Support.Quartz;
+using System.Text.RegularExpressions;
 
 namespace WalkingTec.Mvvm.Mvc
 {
@@ -798,6 +799,18 @@ namespace WalkingTec.Mvvm.Mvc
                         using (var dc = configs.Connections.Where(x => x.Key.ToLower() == "default").FirstOrDefault().CreateDC())
                         {
                             tenants = dc.Set<FrameworkTenant>().IgnoreQueryFilters().Where(x => x.Enabled).ToList();
+                            foreach (var item in tenants)
+                            {
+                                if (string.IsNullOrEmpty(item.TDomain)==false)
+                                {
+                                    Regex r = new Regex("(http://|https://)?(.+)(/)?");
+                                    var m = r.Match(item.TDomain);
+                                    if (m.Success)
+                                    {
+                                        item.TDomain = m.Groups[2].Value;
+                                    }
+                                }
+                            }
                         }
                     }
                     if(tenants == null)
