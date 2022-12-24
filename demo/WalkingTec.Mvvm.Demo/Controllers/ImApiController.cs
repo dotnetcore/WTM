72a9bca9e9827a32a3dcafc725a0c5d6d0e31067
kgxk <34696839+sxy407249209@users.noreply.github.com>
2022-12-24 16:20:36 +0800

    用组通讯方案
    按照wtm自带用户组，进行即时通讯。存在一个问题，租户编号必须存在，且唯一。若开发者采用租户模式开发，则会影响系统混乱。个人认为，应该是在同已租户，不同组之间进行即时通讯。

--- demo/WalkingTec.Mvvm.Demo/Controllers/ImApiController.cs
+++ demo/WalkingTec.Mvvm.Demo/Controllers/ImApiController.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NPOI.SS.Formula.Functions;
 using WalkingTec.Mvvm.Core;
 using WalkingTec.Mvvm.Core.Models;
 using WalkingTec.Mvvm.Demo.Hubs;
@@ -20,6 +21,10 @@ namespace WalkingTec.Mvvm.Demo.Controllers
     [ActionDescription("imapi控制器")]
     public class ImApiController : BaseApiController
     {
+
+
+        #region 采用按wtm自带分组
+
         [HttpGet]
         public async Task<IActionResult> GetinitData()
         {
@@ -36,15 +41,18 @@ namespace WalkingTec.Mvvm.Demo.Controllers
 
             };
             var conn = ConnectionManagement.SignalRConns;
-            var department = await DC.Set<Department>().AsNoTracking().ToListAsync();
+            var frameworkGroup = await DC.Set<FrameworkGroup>().AsNoTracking().ToListAsync();
             List<FriendInfoVM> FriendInfos = new();
-            department.ForEach(async work =>
+            frameworkGroup.ForEach(async group =>
             {
                 FriendInfoVM friendInfo = new();
-                friendInfo.groupname = work.DepName;
-                friendInfo.id = work.ID.ToString();
+                friendInfo.groupname = group.GroupName;
+                friendInfo.id = group.ID.ToString();
+
+                var groupUser = await DC.Set<FrameworkUserGroup>().AsNoTracking().Where(x => x.GroupCode == group.GroupCode).Select(x => x.UserCode).ToArrayAsync();
+
                 var users = await DC.Set<FrameworkUser>().AsNoTracking()
-                .Where(user => user.DepartmentId == work.ID)
+                .Where(x=> groupUser.Any(y=>y== x.ITCode))
                 .Select(user => new ImUserInfoVM
                 {
                     username = user.Name,
@@ -77,5 +85,70 @@ namespace WalkingTec.Mvvm.Demo.Controllers
 
 
         }
+
+
+        #endregion
+
+        #region 用单位获取，暂时弃用
+        //[HttpGet]
+        //public async Task<IActionResult> GetinitData()
+        //{
+
+        //    var userinfo = await DC.Set<FrameworkUser>().AsNoTracking().FirstOrDefaultAsync(x => x.ID == new Guid(Wtm.LoginUserInfo.UserId));
+
+        //    ImUserInfoVM mineinfo = new()
+        //    {
+        //        username = Wtm.LoginUserInfo.Name,
+        //        id = Wtm.LoginUserInfo.UserId,
+        //        status = "online",
+        //        avatar = Wtm.LoginUserInfo.PhotoId != null ? "/api/_file/getuserphoto/" + Wtm.LoginUserInfo.PhotoId : "",
+        //        sign = userinfo.Sign,
+
+        //    };
+        //    var conn = ConnectionManagement.SignalRConns;
+        //    var department = await DC.Set<Department>().AsNoTracking().ToListAsync();
+        //    List<FriendInfoVM> FriendInfos = new();
+        //    department.ForEach(async work =>
+        //    {
+        //        FriendInfoVM friendInfo = new();
+        //        friendInfo.groupname = work.DepName;
+        //        friendInfo.id = work.ID.ToString();
+        //        var users = await DC.Set<FrameworkUser>().AsNoTracking()
+        //        .Where(user => user.DepartmentId == work.ID)
+        //        .Select(user => new ImUserInfoVM
+        //        {
+        //            username = user.Name,
+        //            id = user.ID.ToString().ToLower(),
+        //            status = ConnectionManagement.IsConn(user.ID.ToString()) == true ? "online" : "offline",
+        //            avatar = user.PhotoId != null ? "/api/_file/getuserphoto/" + user.PhotoId : "",
+        //            sign = user.Sign,
+
+        //        })
+        //        .ToListAsync();
+        //        friendInfo.list = users;
+        //        FriendInfos.Add(friendInfo);
+
+        //    });
+
+        //    var imInitUserData = new ImInitUserDataVM
+        //    {
+        //        mine = mineinfo,
+        //        friend = FriendInfos
+        //    };
+
+        //    ImInitDataVM initData = new()
+        //    {
+        //        code = 0,
+        //        msg = "",
+        //        data = imInitUserData
+        //    };
+
+        //    return Ok(initData);
+
+
+        //}
+        #endregion
+
+
     }
 }
