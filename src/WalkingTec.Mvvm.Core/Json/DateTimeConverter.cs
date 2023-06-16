7c7e9cd7f055b3d54cb6c004dcba6c90c340979f
liu liang <liuliang79@live.com>
2023-06-16 11:39:38 +0800

    update datetimeconverter

--- src/WalkingTec.Mvvm.Core/Json/DateTimeConverter.cs
+++ src/WalkingTec.Mvvm.Core/Json/DateTimeConverter.cs
@@ -9,21 +9,26 @@ using System.Threading.Tasks;
 namespace WalkingTec.Mvvm.Core.Json
 {
 
-    public class DateTimeConverter : JsonConverter<DateTime>
+    public class DateTimeConverter : JsonConverter<DateTime?>
     {
-        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             if (reader.TokenType == JsonTokenType.String)
             {
                 if (DateTime.TryParse(reader.GetString(), out DateTime date))
                     return date;
+                else
+                    return null;
             }
             return reader.GetDateTime();
         }
 
-        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(value.ToString("yyyy-MM-dd HH:mm:ss"));
+            if (value != null)
+            {
+                writer.WriteStringValue(value?.ToString("yyyy-MM-dd HH:mm:ss"));
+            }
         }
     }
 }
