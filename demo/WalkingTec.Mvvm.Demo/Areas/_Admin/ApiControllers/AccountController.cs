7224bef99060a0bf8490df48b39fec2b5d2dbde0
liu liang <liuliang79@live.com>
2023-08-24 15:45:45 +0800

    1

--- demo/WalkingTec.Mvvm.Demo/Areas/_Admin/ApiControllers/AccountController.cs
+++ demo/WalkingTec.Mvvm.Demo/Areas/_Admin/ApiControllers/AccountController.cs
@@ -313,7 +313,8 @@ namespace WalkingTec.Mvvm.Admin.Api
             return Ok(users);
         }
 
-        [Public]
+        [AllRights]
+        [HttpGet("GetFrameworkUsers")]
         public IActionResult GetFrameworkUsers()
         {
             return Ok(DC.Set<FrameworkUser>().GetSelectListItems(Wtm, x => x.Name, x => x.ITCode));
